890a5a6447487463c491431f9fb3f800e37dd1a1
Alexey Kadyrov <alexey.kadyrov@r3.com>
2021-07-29 10:19:21 +0100

    CORE-2125 moved cipher suite

---
+++ cipher-suite/src/main/kotlin/net/corda/v5/cipher/suite/DigestAlgorithm.kt
@@ -0,0 +1,24 @@
+package net.corda.v5.cipher.suite
+
+
+/**
+ * Interface for injecting custom digest implementation bypassing JCA.
+ */
+interface DigestAlgorithm {
+    /**
+     * Algorithm identifier.
+     */
+    val algorithm: String
+
+    /**
+     * The length of the digest in bytes.
+     */
+    val digestLength: Int
+
+    /**
+     * Computes the digest of the [ByteArray].
+     *
+     * @param bytes The [ByteArray] to hash.
+     */
+    fun digest(bytes: ByteArray): ByteArray
+}
