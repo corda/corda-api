67294513d3125fb05d54912d66f899d7dc6f553f
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2021-09-07 17:16:39 +0100

    Align kdoc

--- ledger/src/main/kotlin/net/corda/v5/ledger/transactions/PrivacySalt.kt
+++ ledger/src/main/kotlin/net/corda/v5/ledger/transactions/PrivacySalt.kt
@@ -9,9 +9,8 @@ import net.corda.v5.crypto.SecureHash
  * use brute force techniques and reveal the content of a Merkle-leaf hashed value.
  * Because this salt serves the role of the seed to compute nonces, its size and entropy should be equal to the
  * underlying hash function used for Merkle tree generation, currently [SecureHash.SHA256], which has an output of 32 bytes.
- * There are two constructors, one that generates a new 32-bytes random salt, and another that takes a [ByteArray] input.
- * The latter is required in cases where the salt value needs to be pre-generated (agreed between transacting parties),
- * but it is highlighted that one should always ensure it has sufficient entropy.
+ * The salt value needs to be pre-generated (agreed between transacting parties), but it is highlighted that one should always ensure
+ * it has sufficient entropy.
  */
 @CordaSerializable
 class PrivacySalt(bytes: ByteArray) : OpaqueBytes(bytes) {
