71b8a38cedf554da6adccab5e862d453baceacb7
Ryan Fowler <ryan.fowler@r3.com>
2021-11-16 14:50:59 +0000

    CORE-2650: Add in schema structure for virtual node configuration

--- data/config-schema/src/main/kotlin/net/corda/schema/configuration/ConfigKeys.kt
+++ data/config-schema/src/main/kotlin/net/corda/schema/configuration/ConfigKeys.kt
@@ -17,6 +17,7 @@ class ConfigKeys {
         const val IDENTITY_CONFIG = "corda.identity"
         const val MESSAGING_CONFIG = "corda.messaging"
         const val P2P_CONFIG = "corda.p2p"
+        const val PLATFORM_CONFIG = "corda.platform"
         const val POLICY_CONFIG = "corda.policy"
         const val RPC_CONFIG = "corda.rpc"
         const val SECRETS_CONFIG = "corda.secrets"
