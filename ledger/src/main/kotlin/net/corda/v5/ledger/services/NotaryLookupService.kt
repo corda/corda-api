d0253558cf7ec78753feecca201887134a8f4f68
Charlie Crean <charlie.crean@r3.com>
2021-12-08 14:32:07 +0000

    Tidy up

--- ledger/src/main/kotlin/net/corda/v5/ledger/services/NotaryLookupService.kt
+++ ledger/src/main/kotlin/net/corda/v5/ledger/services/NotaryLookupService.kt
@@ -16,18 +16,17 @@ interface NotaryLookupService : CordaFlowInjectable, CordaServiceInjectable {
     /**
      * A list of notary services available on the network.
      *
-     * Note that the services are sorted based on legal name, and the
-     * ordering might change once new notaries are introduced.
+     * Note that the services are sorted based on legal name, and the ordering might change once new notaries are
+     * introduced.
      *
-     * In case of converting a returned list to a string representation
-     * use another delimiter rather than "," or wrap each entry with a
-     * postfix and a prefix:
+     * In case of converting a returned list to a string representation use another delimiter rather than "," or wrap
+     * each entry with a postfix and a prefix:
      * ```
      * list.joinToString("|")
      * list.map { "[$it]" }.toString()
      * ```
-     * This allows to distinguish between each element of a map, as
-     * [Party.toString] returns comma separated values of [CordaX500Name].
+     * This allows to distinguish between each element of a map, as [Party.toString] returns comma separated values of
+     * [CordaX500Name].
      *
      * @return A list of the network's Notary services.
      */
@@ -36,11 +35,9 @@ interface NotaryLookupService : CordaFlowInjectable, CordaServiceInjectable {
     /**
      * Looks up the notary information of a notary by legal name.
      *
-     * @param notaryServiceName The [CordaX500Name] of the notary service
-     * 		to retrieve.
+     * @param notaryServiceName The [CordaX500Name] of the notary service to retrieve.
      *
-     * @return The [NotaryInfo] that matches the input [notaryServiceName],
-     *      or null if no such notary exists.
+     * @return The [NotaryInfo] that matches the input [notaryServiceName], or null if no such notary exists.
      */
     fun lookup(notaryServiceName: CordaX500Name): NotaryInfo?
 
@@ -49,19 +46,16 @@ interface NotaryLookupService : CordaFlowInjectable, CordaServiceInjectable {
      *
      * @param publicKey The [PublicKey] of the notary service to retrieve.
      *
-     * @return The [NotaryInfo] that matches the input [publicKey], or null
-     * 		if no such notary exists.
+     * @return The [NotaryInfo] that matches the input [publicKey], or null if no such notary exists.
      */
     fun lookup(publicKey: PublicKey): NotaryInfo?
 
     /**
-     * Returns true if and only if the given [Party] is a notary, which is
-     * defined by the network parameters.
+     * Returns true if and only if the given [Party] is a notary, which is defined by the network parameters.
      *
      * @param party The [Party] to check.
      *
-     * @return true if the input [party] is a notary, or false if it is not
-     * 		a notary or does not exist.
+     * @return true if the input [party] is a notary, or false if it is not a notary or does not exist.
      */
     fun isNotary(party: Party): Boolean
 }
