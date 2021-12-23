374ea7ecfaf4a2164199d77be0e7175ec5b9a146
joeldudleyr3 <joel.dudley@r3.com>
2021-12-23 15:51:10 +0000

    Adds topics for config management.

---
+++ data/topic-schema/src/main/kotlin/net/corda/config/schema/Schema.kt
@@ -0,0 +1,8 @@
+package net.corda.config.schema
+
+@Suppress("Unused")
+class Schema {
+    companion object {
+        const val CONFIG_MGMT_REQUEST_TOPIC = "config-management-request.topic"
+    }
+}
