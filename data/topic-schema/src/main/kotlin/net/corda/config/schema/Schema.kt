8394b2aa8d9a50d9ad04b612d7574973cd3533e9
joeldudleyr3 <joel.dudley@r3.com>
2022-01-06 09:26:36 +0000

    Address feedback.

--- data/topic-schema/src/main/kotlin/net/corda/config/schema/Schema.kt
+++
@@ -1,8 +0,0 @@
-package net.corda.config.schema
-
-@Suppress("Unused")
-class Schema {
-    companion object {
-        const val CONFIG_MGMT_REQUEST_TOPIC = "config-management-request.topic"
-    }
-}
