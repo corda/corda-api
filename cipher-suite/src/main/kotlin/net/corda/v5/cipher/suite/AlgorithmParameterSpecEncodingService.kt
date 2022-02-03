f033069b3fdadbb90c4218e57e26bdb16a128926
Alexey Kadyrov <alexey.kadyrov@r3.com>
2022-02-03 13:25:11 +0000

    fixed signature of AlgorithmParameterSpecEncodingService

--- cipher-suite/src/main/kotlin/net/corda/v5/cipher/suite/AlgorithmParameterSpecEncodingService.kt
+++ cipher-suite/src/main/kotlin/net/corda/v5/cipher/suite/AlgorithmParameterSpecEncodingService.kt
@@ -1,5 +1,6 @@
 package net.corda.v5.cipher.suite
 
+import net.corda.v5.cipher.suite.schemes.SerializedAlgorithmParameterSpec
 import java.security.spec.AlgorithmParameterSpec
 
 /**
@@ -9,10 +10,10 @@ interface AlgorithmParameterSpecEncodingService {
     /**
      * Serialize the given parameters into the byte array.
      */
-    fun serialize(params: AlgorithmParameterSpec): ByteArray
+    fun serialize(params: AlgorithmParameterSpec): SerializedAlgorithmParameterSpec
 
     /**
      * Deserialize the given byte array into corresponding parameters.
      */
-    fun deserialize(bytes: ByteArray): AlgorithmParameterSpec
+    fun deserialize(params: SerializedAlgorithmParameterSpec): AlgorithmParameterSpec
 }
