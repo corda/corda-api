15ebbfa51c0c75a779fcee0ed14363cfcb6a7728
LWogan <lorcan.wogan@r3.com>
2022-02-18 10:46:15 +0000

    CORE-3904 add default config

--- data/config-schema/src/main/kotlin/net/corda/schema/configuration/FlowConfig.kt
+++ data/config-schema/src/main/kotlin/net/corda/schema/configuration/FlowConfig.kt
@@ -2,5 +2,5 @@ package net.corda.schema.configuration
 
 object FlowConfig {
         const val SESSION_MESSAGE_RESEND_WINDOW = "session.messageResendWindow"
-        const val SESSION_HEARTBEAT_TIMEOUT_WINDOW = "session.heartbeat.timeout"
+        const val SESSION_HEARTBEAT_TIMEOUT_WINDOW = "session.heartbeatTimeout"
 }
