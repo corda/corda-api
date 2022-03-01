205b747eb8de5b8e7e3eff88f4ba545390a48f2f
Alexey Kadyrov <alexey.kadyrov@r3.com>
2022-03-01 16:57:12 +0000

    fixed test

--- membership-identity/src/test/java/net/corda/v5/membership/identity/MemberInfoJavaApiTest.java
+++ membership-identity/src/test/java/net/corda/v5/membership/identity/MemberInfoJavaApiTest.java
@@ -43,7 +43,7 @@ public class MemberInfoJavaApiTest {
 
     @Test
     public void getName() {
-        MemberX500Name testName = mock(MemberX500Name.class);
+        MemberX500Name testName = MemberX500Name.parse("CN=alice, O=R3, L=Dublin, C=IE");
         when(memberInfo.getName()).thenReturn(testName);
 
         MemberX500Name result = memberInfo.getName();
