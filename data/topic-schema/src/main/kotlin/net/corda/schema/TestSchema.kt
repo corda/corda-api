6c1bf2072eaa3a475188da0844d74eccd5feb360
Yiftach Kaplan <yiftach.kaplan@r3.com>
2022-03-09 15:40:04 +0000

    CORE-3758: Split network entries to groups and identities

--- data/topic-schema/src/main/kotlin/net/corda/schema/TestSchema.kt
+++ data/topic-schema/src/main/kotlin/net/corda/schema/TestSchema.kt
@@ -7,7 +7,8 @@ class TestSchema {
          * P2P Topic Test Schema
          */
         const val CRYPTO_KEYS_TOPIC = "p2p.crypto.keys"
-        const val NETWORK_MAP_TOPIC = "p2p.netmap"
+        const val GROUP_NETWORK_MAP_TOPIC = "p2p.group.netmap"
+        const val IDENTITY_NETWORK_MAP_TOPIC = "p2p.identity.netmap"
         const val HOSTED_MAP_TOPIC = "p2p.hosted.identities"
     }
 }
