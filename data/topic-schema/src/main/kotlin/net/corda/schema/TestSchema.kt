7b7cd4f5233c223c883d926a0c4ab82820b8385f
Yiftach Kaplan <yiftach.kaplan@r3.com>
2022-03-09 09:48:46 +0000

    Apply review comments

--- data/topic-schema/src/main/kotlin/net/corda/schema/TestSchema.kt
+++ data/topic-schema/src/main/kotlin/net/corda/schema/TestSchema.kt
@@ -8,6 +8,6 @@ class TestSchema {
          */
         const val CRYPTO_KEYS_TOPIC = "p2p.crypto.keys"
         const val NETWORK_MAP_TOPIC = "p2p.netmap"
-        const val HOSTING_MAP_TOPIC = "p2p.hosting.identities"
+        const val HOSTED_MAP_TOPIC = "p2p.hosted.identities"
     }
 }
