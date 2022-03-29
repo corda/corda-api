c3bfbf2fae7fed86ffdc00273f2d984f26ba3e47
Alexey Kadyrov <alexey.kadyrov@r3.com>
2022-03-29 21:27:12 +0100

    ...

--- cipher-suite/src/main/kotlin/net/corda/v5/cipher/suite/KeyGenerationSpec.kt
+++ cipher-suite/src/main/kotlin/net/corda/v5/cipher/suite/KeyGenerationSpec.kt
@@ -13,7 +13,8 @@ import net.corda.v5.cipher.suite.schemes.SignatureScheme
  * HMAC as the unique alias. The null value indicates that the generated key should be wrapped. The actual implementation
  * may always generate keys persisted in HSM (if that natively supports large number of the keys), so if the alias is null
  * then the implementation will have to generate some unique value by itself, or always generate wrapped keys.
- * @property masterKeyAlias The wrapping key alias which can be used when generating the wrapped keys.
+ * @property masterKeyAlias The wrapping key alias which can be used when generating the wrapped keys, the value
+ * could be null for HSMs which use built-in wrapping keys.
  * @property secret The configured secret that can be used to generate the unique HSM alias.
  *
  * Note about key aliases. Corda always uses single alias to identify a key pair however some HSMs need separate
@@ -24,7 +25,7 @@ class KeyGenerationSpec(
     val tenantId: String,
     val signatureScheme: SignatureScheme,
     val alias: String?,
-    val masterKeyAlias: String,
+    val masterKeyAlias: String?,
     val secret: ByteArray?
 )
 
--- cipher-suite/src/main/kotlin/net/corda/v5/cipher/suite/SigningWrappedSpec.kt
+++ cipher-suite/src/main/kotlin/net/corda/v5/cipher/suite/SigningWrappedSpec.kt
@@ -6,13 +6,14 @@ import net.corda.v5.cipher.suite.schemes.SignatureScheme
  * Holding class for the private key material.
  *
  * @property keyMaterial The encoded and encrypted private key.
- * @property masterKeyAlias The wrapping key's alias which was used for wrapping.
+ * @property masterKeyAlias The wrapping key's alias which was used for wrapping, the value
+ * could be null for HSMs which use built-in wrapping keys.
  * @property encodingVersion The encoding version which was used to encode the private key.
  * @property signatureScheme The scheme for the signing operation.
  */
 class SigningWrappedSpec(
     val keyMaterial: ByteArray,
-    val masterKeyAlias: String,
+    val masterKeyAlias: String?,
     val encodingVersion: Int,
     override val signatureScheme: SignatureScheme
 ) : SigningSpec
