a082f5077c361774664cb3189abd73979a3551e6
LWogan <lorcan.wogan@r3.com>
2022-04-06 11:09:21 +0100

    CORE-3781 config keys for DB

--- data/config-schema/src/main/kotlin/net/corda/schema/configuration/MessagingConfig.kt
+++ data/config-schema/src/main/kotlin/net/corda/schema/configuration/MessagingConfig.kt
@@ -20,15 +20,31 @@ object MessagingConfig {
      * Configuration for connecting to the underlying message bus.
      */
     object Bus {
+        const val MAX_POLL_RECORDS = "max.poll.records"
+        const val AUTO_OFFSET_RESET = "auto.offset.reset"
+        const val MAX_POLL_INTERVAL = "max.poll.interval.ms"
+        const val JDBC_URL = "jdbc.url"
+        const val JDBC_USER = "user"
+        const val JDBC_PASS = "pass"
+
         const val BUS = "bus"
         const val BUS_TYPE = "$BUS.busType"
         const val KAFKA_PROPERTIES = "$BUS.kafkaProperties"
         const val KAFKA_PROPERTIES_COMMON = "$KAFKA_PROPERTIES.common"
         const val BOOTSTRAP_SERVER = "$KAFKA_PROPERTIES_COMMON.bootstrap.servers"
         const val KAFKA_PROPERTIES_CONSUMER = "$KAFKA_PROPERTIES.consumer"
-        const val CONSUMER_MAX_POLL_INTERVAL = "$KAFKA_PROPERTIES_CONSUMER.max.poll.interval.ms"
+        const val KAFKA_CONSUMER_MAX_POLL_INTERVAL = "$KAFKA_PROPERTIES_CONSUMER.$MAX_POLL_INTERVAL"
 
         const val DB_PROPERTIES = "$BUS.dbProperties"
+
+        const val DB_PROPERTIES_COMMON = "$DB_PROPERTIES.common"
+        const val DB_JDBC_URL= "$DB_PROPERTIES_COMMON.$JDBC_URL"
+        const val DB_USER = "$DB_PROPERTIES_COMMON.$JDBC_USER"
+        const val DB_PASS = "$DB_PROPERTIES_COMMON.$JDBC_PASS"
+
+        const val DB_PROPERTIES_CONSUMER = "$DB_PROPERTIES.consumer"
+        const val DB_CONSUMER_MAX_POLL_RECORDS = "$DB_PROPERTIES_CONSUMER.$MAX_POLL_RECORDS"
+        const val DB_CONSUMER_AUTO_OFFSET_RESET = "$DB_PROPERTIES_CONSUMER.$AUTO_OFFSET_RESET"
     }
 
     /**
