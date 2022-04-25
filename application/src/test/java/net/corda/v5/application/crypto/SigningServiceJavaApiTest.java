b9cf18b0f4c4e0a87322af0f05815952663ebe9a
James Higgs <james.higgs@r3.com>
2022-04-25 12:06:35 +0100

    CORE-4676: Fix signing service tests

--- application/src/test/java/net/corda/v5/application/crypto/SigningServiceJavaApiTest.java
+++ application/src/test/java/net/corda/v5/application/crypto/SigningServiceJavaApiTest.java
@@ -1,6 +1,7 @@
 package net.corda.v5.application.crypto;
 
 import net.corda.v5.crypto.DigitalSignature;
+import net.corda.v5.crypto.SignatureSpec;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -14,15 +15,16 @@ class SigningServiceJavaApiTest {
 
     private final SigningService signingService = mock(SigningService.class);
     private final PublicKey publicKey = mock(PublicKey.class);
+    private final SignatureSpec spec = new SignatureSpec("mock", null, null , null);
 
     @Test
     void signWithByteArrayTest() {
         final DigitalSignature.WithKey signatureWithKey = new DigitalSignature.WithKey(publicKey, "test".getBytes());
-        Mockito.when(signingService.sign((byte[]) any(), any())).thenReturn(signatureWithKey);
+        Mockito.when(signingService.sign((byte[]) any(), any(), any())).thenReturn(signatureWithKey);
 
-        Assertions.assertThat(signingService.sign("test".getBytes(), publicKey)).isNotNull();
+        Assertions.assertThat(signingService.sign("test".getBytes(), publicKey, spec)).isNotNull();
         Assertions
-                .assertThat(signingService.sign("test".getBytes(), publicKey))
+                .assertThat(signingService.sign("test".getBytes(), publicKey, spec))
                 .isEqualTo(signatureWithKey);
     }
 }
