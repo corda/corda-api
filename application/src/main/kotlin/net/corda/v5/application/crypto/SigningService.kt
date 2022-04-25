e234cdb37afa1deec8d3518a9b1bae3cb99aff51
James Higgs <james.higgs@r3.com>
2022-04-25 11:58:07 +0100

    CORE-4676: Add signature spec to the sign API

--- application/src/main/kotlin/net/corda/v5/application/crypto/SigningService.kt
+++ application/src/main/kotlin/net/corda/v5/application/crypto/SigningService.kt
@@ -6,6 +6,7 @@ import net.corda.v5.base.annotations.DoNotImplement
 import net.corda.v5.base.annotations.Suspendable
 import net.corda.v5.crypto.CompositeKey
 import net.corda.v5.crypto.DigitalSignature
+import net.corda.v5.crypto.SignatureSpec
 import java.security.KeyPair
 import java.security.PrivateKey
 import java.security.PublicKey
@@ -27,11 +28,13 @@ interface SigningService : CordaServiceInjectable, CordaFlowInjectable {
      * previously generated via the [freshKey] method. If the [PublicKey] is actually a [CompositeKey], the first leaf signing key hosted by
      * the node is used.
      *
+     * @param signatureSpec The [SignatureSpec] to use when producing this signature.
+     *
      * @return A [DigitalSignature.WithKey] representing the signed data and the [PublicKey] that belongs to the same [KeyPair] as the
      * [PrivateKey] that signed the data.
      *
      * @throws IllegalArgumentException If the input key is not a member of [keys].
      */
     @Suspendable
-    fun sign(bytes: ByteArray, publicKey: PublicKey): DigitalSignature.WithKey
+    fun sign(bytes: ByteArray, publicKey: PublicKey, signatureSpec: SignatureSpec): DigitalSignature.WithKey
 }
