7e91926b8475163b4ba568ab5c28d15d72f1764f
LWogan <lorcan.wogan@r3.com>
2022-05-19 08:09:05 +0100

    CORE-3972 boot config updates

--- data/config-schema/src/main/kotlin/net/corda/schema/configuration/ConfigKeys.kt
+++ data/config-schema/src/main/kotlin/net/corda/schema/configuration/ConfigKeys.kt
@@ -44,11 +44,6 @@ object ConfigKeys {
     const val WORKSPACE_DIR = "dir.workspace"
     const val TEMP_DIR = "dir.tmp"
 
-    // Scheduled reconciliation tasks
-    const val RECONCILIATION_PERMISSION_SUMMARY_INTERVAL_MS = "permissionSummaryIntervalMs"
-    const val RECONCILIATION_CPK_WRITE_INTERVAL_MS = "cpkWriteIntervalMs"
-    const val RECONCILIATION_CPI_INFO_INTERVAL_MS = "cpiInfoIntervalMs"
-
     // Sandbox
     const val SANDBOX_CACHE_SIZE = "cache.size"
 
