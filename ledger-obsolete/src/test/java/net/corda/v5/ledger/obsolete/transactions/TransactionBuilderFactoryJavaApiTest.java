68868ea9d61e9962d6928f25a8cd31e9c7b3d1dc
Lajos Veres <lajos.veres@r3.com>
2022-05-31 15:37:00 +0100

    CORE-4984 rename old ledger to free up name

---
+++ ledger-obsolete/src/test/java/net/corda/v5/ledger/obsolete/transactions/TransactionBuilderFactoryJavaApiTest.java
@@ -0,0 +1,25 @@
+package net.corda.v5.ledger.obsolete.transactions;
+
+import net.corda.v5.ledger.obsolete.transactions.TransactionBuilder;
+import net.corda.v5.ledger.obsolete.transactions.TransactionBuilderFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TransactionBuilderFactoryJavaApiTest {
+
+    private final TransactionBuilderFactory transactionBuilderFactory = mock(TransactionBuilderFactory.class);
+    private final TransactionBuilder transactionBuilder = mock(TransactionBuilder.class);
+
+    @Test
+    public void create() {
+        when(transactionBuilderFactory.create()).thenReturn(transactionBuilder);
+
+        TransactionBuilder result = transactionBuilderFactory.create();
+
+        Assertions.assertThat(result).isNotNull();
+        Assertions.assertThat(result).isEqualTo(transactionBuilder);
+    }
+}
