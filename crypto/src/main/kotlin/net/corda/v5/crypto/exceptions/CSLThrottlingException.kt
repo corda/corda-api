d3e0d3e0da2673ec348ee871cb5808afd72a128c
Alexey Kadyrov <alexey.kadyrov@r3.com>
2022-06-05 19:19:39 +0100

    added additional crypto exceptions

--- crypto/src/main/kotlin/net/corda/v5/crypto/exceptions/CSLThrottlingException.kt
+++ crypto/src/main/kotlin/net/corda/v5/crypto/exceptions/CSLThrottlingException.kt
@@ -3,20 +3,10 @@ package net.corda.v5.crypto.exceptions
 import net.corda.v5.base.annotations.CordaSerializable
 
 @CordaSerializable
-open class CSLThrottlingException : CryptoServiceLibraryException {
-    companion object {
-        const val DEFAULT_THROTTLE_INITIAL_BACKOFF = 1_000L
-        const val DEFAULT_THROTTLE_BACKOFF_MULTIPLIER = 2
-    }
-
+abstract class CSLThrottlingException : CryptoServiceLibraryException {
     constructor(message: String) : super(message, isRecoverable = true)
 
     constructor(message: String, cause: Throwable?) : super(message, cause, isRecoverable = true)
 
-    open fun getBackoff(attempt: Int, currentBackoffMillis: Long): Long =
-        if(currentBackoffMillis < DEFAULT_THROTTLE_INITIAL_BACKOFF) {
-            DEFAULT_THROTTLE_INITIAL_BACKOFF
-        } else {
-            currentBackoffMillis * DEFAULT_THROTTLE_BACKOFF_MULTIPLIER
-        }
+    abstract fun getBackoff(attempt: Int, currentBackoffMillis: Long): Long
 }
