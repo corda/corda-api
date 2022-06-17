9806cd8511e01bc9f5475725c5c2d4a827b1a9e9
James Higgs <james.higgs@r3.com>
2022-06-17 17:14:07 +0100

    CORE-5102: Add getRequestBodyAsList method to RPCRequestData

--- application/src/main/kotlin/net/corda/v5/application/flows/RPCRequestData.kt
+++ application/src/main/kotlin/net/corda/v5/application/flows/RPCRequestData.kt
@@ -22,11 +22,43 @@ interface RPCRequestData {
      * The selected marshalling service will determine what format data is returned.
      *
      * @param marshallingService The marshalling service to use to deserialize this request body.
-     * @return JSON representation of the input request body.
+     * @param clazz The class to deserialize the data into
+     * @return An instance of the class populated by the provided input data
      */
     fun <T> getRequestBodyAs(marshallingService: MarshallingService, clazz: Class<T>) : T
+
+    /**
+     * Get the request body and deserialize it into a list of the given type, using a marshalling service.
+     *
+     * The selected marshalling service will determine what format data is returned.
+     *
+     * @param marshallingService The marshalling service to use to deserialize this request body.
+     * @param clazz The class to deserialize the data into
+     * @return A list of instances of the class populated by the provided input data
+     */
+    fun <T> getRequestBodyAsList(marshallingService: MarshallingService, clazz: Class<T>) : List<T>
 }
 
+/**
+ * Get the request body and deserialize it into the given type, using a marshalling service.
+ *
+ * The selected marshalling service will determine what format data is returned.
+ *
+ * @param marshallingService The marshalling service to use to deserialize this request body.
+ * @return An instance of the class populated by the provided input data
+ */
 inline fun <reified T> RPCRequestData.getRequestBodyAs(marshallingService: MarshallingService) : T {
     return getRequestBodyAs(marshallingService, T::class.java)
+}
+
+/**
+ * Get the request body and deserialize it into a list of the given type, using a marshalling service.
+ *
+ * The selected marshalling service will determine what format data is returned.
+ *
+ * @param marshallingService The marshalling service to use to deserialize this request body.
+ * @return A list of instances of the class populated by the provided input data
+ */
+inline fun <reified T> RPCRequestData.getRequestBodyAsList(marshallingService: MarshallingService) : List<T> {
+    return getRequestBodyAsList(marshallingService, T::class.java)
 }
