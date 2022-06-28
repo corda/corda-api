2a3591a1a78ae790e1492f5b0cdc89a664a563ec
Oisín Mac Fhearaí <oisin.macfhearai@r3.com>
2022-06-28 18:01:25 +0100

    Add stub Javadoc for ConsensualLedgerService.

--- ledger/ledger-consensual/src/main/kotlin/net/corda/v5/ledger/consensual/ConsensualLedgerService.kt
+++ ledger/ledger-consensual/src/main/kotlin/net/corda/v5/ledger/consensual/ConsensualLedgerService.kt
@@ -3,6 +3,10 @@ package net.corda.v5.ledger.consensual
 import net.corda.v5.base.annotations.DoNotImplement
 import net.corda.v5.base.annotations.Suspendable
 
+/**
+ * A service providing a ledger that operates based on consensual states rather than
+ * the UTXO model.
+ */
 @DoNotImplement
 interface ConsensualLedgerService {
     @Suspendable
