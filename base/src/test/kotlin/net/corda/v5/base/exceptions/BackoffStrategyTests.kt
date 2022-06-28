a19d55660f2ca8bc8e6c1ef7770cc66a8ec5ee1b
Alexey Kadyrov <alexey.kadyrov@r3.com>
2022-06-28 10:29:58 +0100

    fixed detekt

--- base/src/test/kotlin/net/corda/v5/base/exceptions/BackoffStrategyTests.kt
+++ base/src/test/kotlin/net/corda/v5/base/exceptions/BackoffStrategyTests.kt
@@ -10,8 +10,7 @@ class BackoffStrategyTests {
     @Test
     fun `Should return default linear backoff`() {
         val strategy =  createLinearBackoff()
-        var backoff = 0L
-        backoff = strategy.getBackoff(1)
+        var backoff = strategy.getBackoff(1)
         assertEquals(200L, backoff)
         backoff = strategy.getBackoff(2)
         assertEquals(200L, backoff)
@@ -22,8 +21,7 @@ class BackoffStrategyTests {
     @Test
     fun `Should return customizes backoff`() {
         val strategy =  createBackoff(3, listOf(100, 200))
-        var backoff = 0L
-        backoff = strategy.getBackoff(1)
+        var backoff = strategy.getBackoff(1)
         assertEquals(100L, backoff)
         backoff = strategy.getBackoff(2)
         assertEquals(200L, backoff)
@@ -34,8 +32,7 @@ class BackoffStrategyTests {
     @Test
     fun `Should return customizes backoff with repeating value`() {
         val strategy = createBackoff(3, listOf(300))
-        var backoff = 0L
-        backoff = strategy.getBackoff(1)
+        var backoff = strategy.getBackoff(1)
         assertEquals(300L, backoff)
         backoff = strategy.getBackoff(2)
         assertEquals(300L, backoff)
@@ -60,8 +57,7 @@ class BackoffStrategyTests {
     @Test
     fun `Should return customizes backoff for attempts 2 and empty list`() {
         val strategy =  createBackoff(2, emptyList())
-        var backoff = 0L
-        backoff = strategy.getBackoff(1)
+        var backoff = strategy.getBackoff(1)
         assertEquals(0L, backoff)
         backoff = strategy.getBackoff(2)
         assertEquals(-1L, backoff)
@@ -70,8 +66,7 @@ class BackoffStrategyTests {
     @Test
     fun `Should return default exponential backoff`() {
         val strategy = createExponentialBackoff()
-        var backoff = 0L
-        backoff = strategy.getBackoff(1)
+        var backoff = strategy.getBackoff(1)
         assertEquals(1000L, backoff)
         backoff = strategy.getBackoff(2)
         assertEquals(2000L, backoff)
@@ -88,8 +83,7 @@ class BackoffStrategyTests {
     @Test
     fun `Should return customizes exponential backoff`() {
         val strategy = createExponentialBackoff(4, 3000L)
-        var backoff = 0L
-        backoff = strategy.getBackoff(1)
+        var backoff = strategy.getBackoff(1)
         assertEquals(3000L, backoff)
         backoff = strategy.getBackoff(2)
         assertEquals(6000L, backoff)
