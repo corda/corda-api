f9c13a9893fdd893c98418affe49ffd3d9662f57
Lajos Veres <lajos.veres@r3.com>
2022-06-29 10:08:55 +0100

    CORE-5145 Merkle Service - shorten names

--- crypto/src/main/kotlin/net/corda/v5/crypto/merkle/MerkleService.kt
+++ crypto/src/main/kotlin/net/corda/v5/crypto/merkle/MerkleService.kt
@@ -4,18 +4,18 @@ import net.corda.v5.crypto.DigestAlgorithmName
 import net.corda.v5.crypto.SecureHash
 
 interface MerkleService {
-    fun createMerkleTree(
+    fun createTree(
         leaves: List<ByteArray>,
         digestProvider: MerkleTreeHashDigestProvider
     ) : MerkleTree
 
-    fun createMerkleProof(
+    fun createProof(
         treeSize: Int,
         leaves: List<IndexedMerkleLeaf>,
         hashes: List<SecureHash>
     ) : MerkleProof
 
-    fun createMerkleTreeHashDigestProvider(
+    fun createHashDigestProvider(
         merkleTreeHashDigestProviderName: String,
         digestAlgorithmName: DigestAlgorithmName,
         options: HashMap<String, Any>? = null,
