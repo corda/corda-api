6bba7055ec348cb388702049071f1361606e5203
Lajos Veres <lajos.veres@r3.com>
2022-07-01 11:51:19 +0100

    CORE-5145 Merkle some docs

--- crypto/src/main/kotlin/net/corda/v5/crypto/merkle/MerkleTreeFactory.kt
+++ crypto/src/main/kotlin/net/corda/v5/crypto/merkle/MerkleTreeFactory.kt
@@ -3,18 +3,50 @@ package net.corda.v5.crypto.merkle
 import net.corda.v5.crypto.DigestAlgorithmName
 import net.corda.v5.crypto.SecureHash
 
+/**
+ * [MerkleTreeFactory] creates [MerkleTree]s, [MerkleProof]s and [MerkleTreeHashDigestProvider].
+ */
 interface MerkleTreeFactory {
+    /**
+     * Creates a [MerkleTree]
+     *
+     * @param leaves The leaves of the tree.
+     * @param digestProvider Merkle Tree Hash digest provider used to construct the tree's node and leaf hashes.
+     *
+     * @return A new [MerkleTree] instance.
+     */
     fun createTree(
         leaves: List<ByteArray>,
         digestProvider: MerkleTreeHashDigestProvider
     ) : MerkleTree
 
+    /**
+     * Recreates a [MerkleProof] from its data content.
+     * Use [net.corda.v5.crypto.merkle.MerkleTree.createAuditProof] to create a proof for a set of leaves for an
+     * existing [MerkleTree].
+     * Use this function to recreate the [MerkleProof] when the [MerkleTree] is not available.
+     *
+     * @param treeSize Number of leaves in the whole tree
+     * @param leaves Leaf items whose inclusion is proved by the proof.
+     * @param hashes The helper hashes needed to reconstruct the whole tree.
+     *
+     * @return A new [MerkleProof] instance.
+     */
     fun createProof(
         treeSize: Int,
         leaves: List<IndexedMerkleLeaf>,
         hashes: List<SecureHash>
     ) : MerkleProof
 
+    /**
+     * Creates a [MerkleTreeHashDigestProvider].
+     *
+     * @param merkleTreeHashDigestProviderName name of the Hash Digest Provider class
+     * @param digestAlgorithmName name of the base Hash algorithm
+     * @param options Hash Digest provider specific options
+     *
+     * @return A new [MerkleTreeHashDigestProvider] instance.
+     */
     fun createHashDigestProvider(
         merkleTreeHashDigestProviderName: String,
         digestAlgorithmName: DigestAlgorithmName,
--- crypto/src/main/kotlin/net/corda/v5/crypto/merkle/MerkleTreeHashDigestProvider.kt
+++ crypto/src/main/kotlin/net/corda/v5/crypto/merkle/MerkleTreeHashDigestProvider.kt
@@ -3,6 +3,15 @@ package net.corda.v5.crypto.merkle
 import net.corda.v5.crypto.DigestAlgorithmName
 import net.corda.v5.crypto.SecureHash
 
+/**
+ * Different use cases require different [MerkleTree] calculations.
+ * [MerkleTreeHashDigestProvider]s let us specify the
+ *  - Leaf Nonce
+ *  - Leaf Hash
+ *  - Node Hash calculation methods and
+ *  - the base Digest Algorithm.
+ */
+
 interface MerkleTreeHashDigestProvider {
     val digestAlgorithmName: DigestAlgorithmName
 
