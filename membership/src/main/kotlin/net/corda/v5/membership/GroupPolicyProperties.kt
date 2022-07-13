1eb0f0fa43c5b7347828c037f59f4174e02c364a
Yiftach Kaplan <yiftach.kaplan@r3.com>
2022-07-13 11:14:47 +0100

    Apply review comments

--- membership/src/main/kotlin/net/corda/v5/membership/GroupPolicyProperties.kt
+++
@@ -1,8 +0,0 @@
-package net.corda.v5.membership
-
-import net.corda.v5.base.types.LayeredPropertyMap
-
-/**
- * The properties of the group policy
- */
-interface GroupPolicyProperties : LayeredPropertyMap
