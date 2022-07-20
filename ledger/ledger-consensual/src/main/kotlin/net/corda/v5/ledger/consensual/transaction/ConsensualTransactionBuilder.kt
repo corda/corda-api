b8fb6cd87e6a9f4c431d7a9b5202ff9e9f0643f1
Lajos Veres <lajos.veres@r3.com>
2022-07-20 17:44:57 +0100

    CORE-5001: ConsensualTransactionBuilder

---
+++ ledger/ledger-consensual/src/main/kotlin/net/corda/v5/ledger/consensual/transaction/ConsensualTransactionBuilder.kt
@@ -0,0 +1,31 @@
+package net.corda.v5.ledger.consensual.transaction
+
+import net.corda.v5.base.annotations.DoNotImplement
+import net.corda.v5.crypto.DigestService
+import net.corda.v5.crypto.merkle.MerkleTreeFactory
+import net.corda.v5.ledger.common.transactions.PrivacySalt
+import net.corda.v5.ledger.consensual.ConsensualState
+import java.security.PublicKey
+import java.security.SecureRandom
+import java.time.Instant
+
+@DoNotImplement
+interface ConsensualTransactionBuilder {
+    val metadata: ConsensualTransactionMetaData?
+    fun withMetadata(metadata: ConsensualTransactionMetaData): ConsensualTransactionBuilder
+
+    val timeStamp: Instant?
+    fun withTimeStamp(timeStamp: Instant): ConsensualTransactionBuilder
+
+    val requiredSigners: List<PublicKey>
+    fun withRequiredSigner(requiredSigner: PublicKey) : ConsensualTransactionBuilder
+
+    val consensualStates: List<ConsensualState>
+    fun withConsensualState(consensualState: ConsensualState) : ConsensualTransactionBuilder
+
+    fun build(
+        merkleTreeFactory: MerkleTreeFactory,
+        digestService: DigestService,
+        secureRandom: SecureRandom
+    ): ConsensualWireTransaction
+}
