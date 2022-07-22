efb4d0af4cd9e6cbfc95aaa7d2d0ff0a0790890a
Lajos Veres <lajos.veres@r3.com>
2022-07-22 14:31:10 +0100

    CORE-5001 Consensul Tx Metadata CordaSerializable

--- ledger/ledger-common/src/main/kotlin/net/corda/v5/ledger/common/transactions/CpkIdentifier.kt
+++ ledger/ledger-common/src/main/kotlin/net/corda/v5/ledger/common/transactions/CpkIdentifier.kt
@@ -1,6 +1,9 @@
 package net.corda.v5.ledger.common.transactions
 
+import net.corda.v5.base.annotations.CordaSerializable
+
 //TODO: Mainly from [net.corda.libs.cpiupload.endpoints.v1.CpkIdentifier] Clarify their relationship
+@CordaSerializable
 data class CpkIdentifier(
     val name : String,
     val version : String,
--- ledger/ledger-common/src/main/kotlin/net/corda/v5/ledger/common/transactions/TransactionMetaData.kt
+++ ledger/ledger-common/src/main/kotlin/net/corda/v5/ledger/common/transactions/TransactionMetaData.kt
@@ -1,8 +1,10 @@
 package net.corda.v5.ledger.common.transactions
 
+import net.corda.v5.base.annotations.CordaSerializable
 import net.corda.v5.base.annotations.DoNotImplement
 
 @DoNotImplement
+@CordaSerializable
 interface TransactionMetaData {
     val ledgerModel: String
     val ledgerVersion: String
