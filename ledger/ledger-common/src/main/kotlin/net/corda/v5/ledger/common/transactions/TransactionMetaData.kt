063dfb09f558bf832bd8ca52642e716b64d20134
Lajos Veres <lajos.veres@r3.com>
2022-07-22 14:32:02 +0100

    CORE-5001 Consensul Tx Metadata todo

--- ledger/ledger-common/src/main/kotlin/net/corda/v5/ledger/common/transactions/TransactionMetaData.kt
+++ ledger/ledger-common/src/main/kotlin/net/corda/v5/ledger/common/transactions/TransactionMetaData.kt
@@ -3,6 +3,10 @@ package net.corda.v5.ledger.common.transactions
 import net.corda.v5.base.annotations.CordaSerializable
 import net.corda.v5.base.annotations.DoNotImplement
 
+/**
+ * TODO(Design question: Maybe just a simple map to make future changes easier?, Or just adding an options map to it?)
+ */
+
 @DoNotImplement
 @CordaSerializable
 interface TransactionMetaData {
