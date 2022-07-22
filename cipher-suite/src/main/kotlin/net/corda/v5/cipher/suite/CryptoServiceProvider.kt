4fc3f168ff67f3f76899d1b2d6b8dcca1772d8d4
Alexey Kadyrov <alexey.kadyrov@r3.com>
2022-07-22 16:50:49 +0100

    wip

--- cipher-suite/src/main/kotlin/net/corda/v5/cipher/suite/CryptoServiceProvider.kt
+++ cipher-suite/src/main/kotlin/net/corda/v5/cipher/suite/CryptoServiceProvider.kt
@@ -18,8 +18,22 @@ interface CryptoServiceProvider<T : Any> {
     /**
      * Returns an instance of the [CryptoService].
      * @param config crypto service configuration
+     * @param secrets provides access to decrypting the secrets
+     *
+     * The secrets have to be declared as Map<String, Any> in the corresponding POJO, the JSON will look like
+     * in the example bellow for the property called 'passphrase'
+     *
+     * POJO (Kotlin):
+     *  val passphrase: Map<String, Any>
+     *
+     * JSON:
+     *  "passphrase": {
+     *      "configSecret": {
+     *          "encryptedSecret": "<encrypted-value>"
+     *      }
+     *  }
      *
      * @throws [net.corda.v5.crypto.failures.CryptoException] for general cryptographic exceptions.
      */
-    fun getInstance(config: T): CryptoService
+    fun getInstance(config: T, secrets: ConfigurationSecrets): CryptoService
 }
