f1b8be070d97ddd158f94435fcb025744ac72720
Lajos Veres <lajos.veres@r3.com>
2022-07-27 15:48:19 +0100

    CORE-5001 ConsensualTransactionBuilder transition from build(): WireTransaction to sign(): SignedTransaction

--- ledger/ledger-consensual/src/main/kotlin/net/corda/v5/ledger/consensual/transaction/ConsensualTransactionBuilder.kt
+++ ledger/ledger-consensual/src/main/kotlin/net/corda/v5/ledger/consensual/transaction/ConsensualTransactionBuilder.kt
@@ -1,26 +1,29 @@
 package net.corda.v5.ledger.consensual.transaction
 
+import net.corda.v5.application.crypto.SigningService
 import net.corda.v5.application.serialization.SerializationService
 import net.corda.v5.base.annotations.DoNotImplement
 import net.corda.v5.crypto.DigestService
 import net.corda.v5.crypto.merkle.MerkleTreeFactory
-import net.corda.v5.ledger.common.transactions.WireTransaction
+import net.corda.v5.ledger.common.transactions.SignedTransaction
 import net.corda.v5.ledger.consensual.ConsensualState
+import java.security.PublicKey
 import java.security.SecureRandom
 import java.time.Instant
 
 @DoNotImplement
 interface ConsensualTransactionBuilder {
+    val merkleTreeFactory: MerkleTreeFactory
+    val digestService: DigestService
+    val secureRandom: SecureRandom
+    val serializer: SerializationService
+    val signingService: SigningService
+
     val timeStamp: Instant?
     fun withTimeStamp(timeStamp: Instant): ConsensualTransactionBuilder
 
     val consensualStates: List<ConsensualState>
     fun withConsensualState(consensualState: ConsensualState) : ConsensualTransactionBuilder
 
-    fun build(
-        merkleTreeFactory: MerkleTreeFactory,
-        digestService: DigestService,
-        secureRandom: SecureRandom,
-        serializer: SerializationService
-    ): WireTransaction
+    fun sign(publicKey: PublicKey): SignedTransaction
 }
