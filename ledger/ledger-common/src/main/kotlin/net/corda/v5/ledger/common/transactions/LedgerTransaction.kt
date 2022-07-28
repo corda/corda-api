42bfa24b2271295d2ea21a7c465f8852361bb918
Lajos Veres <lajos.veres@r3.com>
2022-07-28 15:48:22 +0100

    CORE-5001 LedgerTransaction does not need to be CordaSerializable

--- ledger/ledger-common/src/main/kotlin/net/corda/v5/ledger/common/transactions/LedgerTransaction.kt
+++ ledger/ledger-common/src/main/kotlin/net/corda/v5/ledger/common/transactions/LedgerTransaction.kt
@@ -1,12 +1,10 @@
 package net.corda.v5.ledger.common.transactions
 
-import net.corda.v5.base.annotations.CordaSerializable
 import net.corda.v5.base.annotations.DoNotImplement
 import net.corda.v5.crypto.SecureHash
 import java.security.PublicKey
 
 @DoNotImplement
-@CordaSerializable // TODO(???)
 interface LedgerTransaction {
     val id: SecureHash
     val wireTransaction: WireTransaction
