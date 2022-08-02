171c75589263c5acce58d48cc079d0ba070f95f8
Lajos Veres <lajos.veres@r3.com>
2022-08-02 10:20:06 +0100

    CORE-5001 Get rid of common/LedgerTransaction for now

--- ledger/ledger-consensual/src/main/kotlin/net/corda/v5/ledger/consensual/transaction/ConsensualLedgerTransaction.kt
+++ ledger/ledger-consensual/src/main/kotlin/net/corda/v5/ledger/consensual/transaction/ConsensualLedgerTransaction.kt
@@ -1,12 +1,15 @@
 package net.corda.v5.ledger.consensual.transaction
 
 import net.corda.v5.base.annotations.DoNotImplement
-import net.corda.v5.ledger.common.transactions.LedgerTransaction
+import net.corda.v5.crypto.SecureHash
 import net.corda.v5.ledger.consensual.ConsensualState
+import java.security.PublicKey
 import java.time.Instant
 
 @DoNotImplement
-interface ConsensualLedgerTransaction: LedgerTransaction {
+interface ConsensualLedgerTransaction {
+    val id: SecureHash
+    val requiredSigningKeys: Set<PublicKey>
     val timestamp: Instant
     val states: List<ConsensualState>
 }
