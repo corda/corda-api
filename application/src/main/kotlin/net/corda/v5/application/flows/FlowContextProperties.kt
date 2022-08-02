37cd966e863ab2f5732be9a694e9fe1beab0859b
Simon Johnson <simon.johnson@r3.com>
2022-08-02 15:51:13 +0100

    CORE-5991 Flow Context propagation API readiness
    All Api changes required for Flow Worker Context Propagation

--- application/src/main/kotlin/net/corda/v5/application/flows/FlowContextProperties.kt
+++ application/src/main/kotlin/net/corda/v5/application/flows/FlowContextProperties.kt
@@ -2,16 +2,44 @@ package net.corda.v5.application.flows
 
 /**
  * Context properties of a [Flow] are key value pairs of Strings. They are comprised of two sets of properties, those
- * set by the platform when a [Flow] is started, and those which are added by the CorDapp developer. Properties are
- * immutable once set. Where keys exist as platform properties already, setting 'user' properties is an effective no-op
- * as they will always be ignored in preference for the platform value.
+ * set by the platform when a [Flow] is started, and those which are added by the CorDapp developer during the execution
+ * of their flow. The latter set are referred to as user context properties. All context properties are immutable once
+ * set.
+ * Where keys exist as platform properties already, setting user properties is an effective no-o as they will always be
+ * ignored in preference for the platform value.
  * Both sets of context properties are passed from the originating [Flow] to sub-flows, initiated flows, and services.
- * Where sub-flows and initiated flows have extra 'user' properties added, these are only visible in the scope of those
+ * Where sub-flows and initiated flows have extra user properties added, these are only visible in the scope of those
  * flows and any of their sub-flows, initiated flows or services, but not back up the flow stack to any flows which
  * sit above the one in which they were added. In this way context properties can also be thought of as a stack,
  * unwinding along with the flow stack.
  */
 interface FlowContextProperties {
-    fun set(key: String, value: String)
-    fun get(key: String): String?
+    /**
+     * Puts a user value into the context property store.
+     * If an existing user value exists anywhere in the current context stack, this value will overwrite it from this
+     * point on down, but not up, the context stack.
+     * If an existing platform value exists anywhere in the context stack, the value is ignored in preference for the
+     * platform value.
+     *
+     * @param key The property key
+     * @param value The property value
+     */
+    fun put(key: String, value: String)
+
+    /**
+     * Gets a value from the context property store.
+     * Also opens up [] operator access for getting values in Kotlin.
+     *
+     * @param key The property key
+     */
+    operator fun get(key: String): String?
+
+    /**
+     * Puts a user value into the context property store.
+     * Opens up [] operator access for setting values in Kotlin.
+     *
+     * @param key The property key
+     * @param value The property value
+     */
+    operator fun set(key: String, value: String)
 }
