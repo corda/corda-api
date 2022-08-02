8261e1d565c8ffb356cf66088bfb379e08e39ae6
Lajos Veres <lajos.veres@r3.com>
2022-08-02 14:56:07 +0100

    CORE-5001 ConsensualLedgerService.getTransactionBuilder() services will be injected as contstructor params so not needed here

--- ledger/ledger-consensual/src/main/kotlin/net/corda/v5/ledger/consensual/ConsensualLedgerService.kt
+++ ledger/ledger-consensual/src/main/kotlin/net/corda/v5/ledger/consensual/ConsensualLedgerService.kt
@@ -1,13 +1,8 @@
 package net.corda.v5.ledger.consensual
 
-import net.corda.v5.application.crypto.SigningService
-import net.corda.v5.application.serialization.SerializationService
 import net.corda.v5.base.annotations.DoNotImplement
 import net.corda.v5.base.annotations.Suspendable
-import net.corda.v5.crypto.DigestService
-import net.corda.v5.crypto.merkle.MerkleTreeFactory
 import net.corda.v5.ledger.consensual.transaction.ConsensualTransactionBuilder
-import java.security.SecureRandom
 
 /**
  * It provides access to the different Consensual Ledger Services
@@ -15,13 +10,7 @@ import java.security.SecureRandom
 @DoNotImplement
 interface ConsensualLedgerService {
     @Suspendable
-    fun getTransactionBuilder(
-        merkleTreeFactory: MerkleTreeFactory,
-        digestService: DigestService,
-        secureRandom: SecureRandom,
-        serializer: SerializationService,
-        signingService: SigningService
-    ): ConsensualTransactionBuilder
+    fun getTransactionBuilder(): ConsensualTransactionBuilder
 
     /* TODO
     fun fetchTransaction(id: SecureHash)
