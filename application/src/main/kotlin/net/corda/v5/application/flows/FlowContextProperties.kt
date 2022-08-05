ead048a9766e83a616b212d9a0e40adf5220ccae
Simon Johnson <simon.johnson@r3.com>
2022-08-05 10:45:56 +0100

    review comments

--- application/src/main/kotlin/net/corda/v5/application/flows/FlowContextProperties.kt
+++ application/src/main/kotlin/net/corda/v5/application/flows/FlowContextProperties.kt
@@ -5,25 +5,23 @@ package net.corda.v5.application.flows
  * set by the platform when a [Flow] is started, and those which are added by the CorDapp developer during the execution
  * of their flow. The latter set are referred to as user context properties. All context properties are immutable once
  * set.
- *
- * Where keys exist as platform properties already, setting user properties with the same key will throw. It is highly
- * advisable to scope user property keys with some unique prefix, e.g. package name. Corda's platform keys will usually
- * be prefixed with "corda." which is reserved, meaning that an attempt to prefix a user key with "corda." will also
- * throw whether it exists already or not.
- *
- * Both sets of context properties are passed from the originating [Flow] to sub-flows, initiated flows, and services.
- * Where sub-flows and initiated flows have extra user properties added, these are only visible in the scope of those
- * flows and any of their sub-flows, initiated flows or services, but not back up the flow stack to any flows which
- * sit above the one in which they were added. In this way context properties can also be thought of as a stack,
- * unwinding along with the flow stack.
  */
 interface FlowContextProperties {
     /**
      * Puts a user value into the context property store.
-     * If an existing user value exists anywhere in the current context stack, this value will overwrite it from this
-     * point on down, but not up, the context stack.
-     * If an existing platform value exists anywhere in the context stack, the value is ignored in preference for the
-     * platform value.
+     *
+     * Where keys exist as platform properties already, setting user properties with the same key will throw. It is
+     * highly advisable to scope user property keys with some unique prefix, e.g. package name. Corda's platform keys
+     * will usually be prefixed with "corda." which is reserved, meaning that an attempt to prefix a user key with
+     * "corda." will also throw whether it exists already or not.
+     *
+     * Both sets of context properties are propagated automatically from the originating [Flow] to all sub-flows
+     * initiated flows, and services. Where sub-flows and initiated flows have extra user properties added, these are
+     * only visible in the scope of those flows and any of their sub-flows, initiated flows or services, but not back up
+     * the flow stack to any flows which launched or initiated those flows. The same is true of overwritten user
+     * properties. These properties are overwritten for the current flow and any flows initiated or instantiated by that
+     * flow, and also any further down the chain. When execution returns to a flow higher up the stack (a parent or one
+     * that initiated it) the overwritten properties are not visible.
      *
      * @param key The property key
      * @param value The property value
@@ -44,10 +42,19 @@ interface FlowContextProperties {
 
 /**
  * Puts a user value into the context property store.
- * If an existing user value exists anywhere in the current context stack, this value will overwrite it from this
- * point on down, but not up, the context stack.
- * If an existing platform value exists anywhere in the context stack, the value is ignored in preference for the
- * platform value.
+ *
+ * Where keys exist as platform properties already, setting user properties with the same key will throw. It is highly
+ * advisable to scope user property keys with some unique prefix, e.g. package name. Corda's platform keys will usually
+ * be prefixed with "corda." which is reserved, meaning that an attempt to prefix a user key with "corda." will also
+ * throw whether it exists already or not.
+ *
+ * Both sets of context properties are propagated automatically from the originating [Flow] to all sub-flows, initiated
+ * flows, and services. Where sub-flows and initiated flows have extra user properties added, these are only visible in
+ * the scope of those flows and any of their sub-flows, initiated flows or services, but not back up the flow stack to
+ * any flows which launched or initiated those flows. The same is true of overwritten user properties. These properties
+ * are overwritten for the current flow and any flows initiated or instantiated by that flow, and also any further down
+ * the chain. When execution returns to a flow higher up the stack (a parent or one that initiated it) the overwritten
+ * properties are not visible.
  *
  * Opens up [] operator access for setting values in Kotlin.
  *
