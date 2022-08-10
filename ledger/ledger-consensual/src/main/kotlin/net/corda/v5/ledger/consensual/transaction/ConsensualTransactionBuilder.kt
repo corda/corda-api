e4c90d75494dbaa3e7327c4958d0d2991eb726a7
Lajos Veres <lajos.veres@r3.com>
2022-08-10 14:35:48 +0100

    CORE-5001 ConsensualTransactionBuilder dependencies do not need to be exposed

--- ledger/ledger-consensual/src/main/kotlin/net/corda/v5/ledger/consensual/transaction/ConsensualTransactionBuilder.kt
+++ ledger/ledger-consensual/src/main/kotlin/net/corda/v5/ledger/consensual/transaction/ConsensualTransactionBuilder.kt
@@ -1,23 +1,12 @@
 package net.corda.v5.ledger.consensual.transaction
 
-import net.corda.v5.application.crypto.SigningService
-import net.corda.v5.application.serialization.SerializationService
 import net.corda.v5.base.annotations.DoNotImplement
-import net.corda.v5.crypto.DigestService
-import net.corda.v5.crypto.merkle.MerkleTreeFactory
 import net.corda.v5.ledger.consensual.ConsensualState
 import java.security.PublicKey
-import java.security.SecureRandom
 import java.time.Instant
 
 @DoNotImplement
 interface ConsensualTransactionBuilder {
-    val merkleTreeFactory: MerkleTreeFactory
-    val digestService: DigestService
-    val secureRandom: SecureRandom
-    val serializer: SerializationService
-    val signingService: SigningService
-
     val timeStamp: Instant?
     fun withTimeStamp(timeStamp: Instant): ConsensualTransactionBuilder
 
