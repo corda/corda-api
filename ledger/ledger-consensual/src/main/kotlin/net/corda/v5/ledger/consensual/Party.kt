acd241999183250d6945cbd3885831043a48ecb9
Lajos Veres <lajos.veres@r3.com>
2022-08-11 16:59:48 +0100

    CORE-5001 Convert Party back

--- ledger/ledger-consensual/src/main/kotlin/net/corda/v5/ledger/consensual/Party.kt
+++ ledger/ledger-consensual/src/main/kotlin/net/corda/v5/ledger/consensual/Party.kt
@@ -1,7 +1,6 @@
 package net.corda.v5.ledger.consensual
 
 import net.corda.v5.base.annotations.CordaSerializable
-import net.corda.v5.base.annotations.DoNotImplement
 import net.corda.v5.base.types.MemberX500Name
 import net.corda.v5.crypto.toStringShort
 import java.security.PublicKey
@@ -9,21 +8,28 @@ import java.security.PublicKey
 /**
  * Representation of a Party.
  */
-@DoNotImplement
 @CordaSerializable
-interface Party {
+class Party(
     /**
      * @property name X500 Name of the party.
      */
-    val name: MemberX500Name
-
+    val name: MemberX500Name,
     /**
      * @property owningKey The [PublicKey] owned by the party.
      */
     val owningKey: PublicKey
+) {
 
     /**
      * Concise, readable String representation of a Party.
      */
     val description: String get() = "$name (owningKey = ${owningKey.toStringShort()})"
+
+    override fun toString() = name.toString()
+    override fun equals(other: Any?): Boolean =
+        other === this ||
+                other is Party &&
+                other.owningKey == owningKey &&
+                other.name == name
+    override fun hashCode(): Int = owningKey.hashCode()
 }
