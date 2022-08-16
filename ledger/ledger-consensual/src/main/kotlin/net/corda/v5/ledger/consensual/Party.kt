254d953f2e1d9e077a01f0359025f88adb2f041d
Lajos Veres <lajos.veres@r3.com>
2022-08-16 09:44:17 +0100

    CORE-5001 Remove CordaSerializable to let custom PartySerializer take over its responsibilities

--- ledger/ledger-consensual/src/main/kotlin/net/corda/v5/ledger/consensual/Party.kt
+++ ledger/ledger-consensual/src/main/kotlin/net/corda/v5/ledger/consensual/Party.kt
@@ -1,6 +1,5 @@
 package net.corda.v5.ledger.consensual
 
-import net.corda.v5.base.annotations.CordaSerializable
 import net.corda.v5.base.annotations.DoNotImplement
 import net.corda.v5.base.types.MemberX500Name
 import net.corda.v5.crypto.toStringShort
@@ -8,9 +7,13 @@ import java.security.PublicKey
 
 /**
  * Representation of a Party.
+ *
+ *
+ * Since used in [ConsensualState] interface, their implementors will depend on it.
+ * To make those implementations serializable, we use a custom serializer in the background, so
+ * the normal @CordaSerializable cannot be used here.
  */
 @DoNotImplement
-@CordaSerializable
 interface Party {
     /**
      * @property name X500 Name of the party.
