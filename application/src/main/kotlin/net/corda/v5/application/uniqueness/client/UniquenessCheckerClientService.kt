78f4ac382157de7adcfb4dfe3b2ea6e3ce65f093
nkovacsx <nandor.kovacs@r3.com>
2022-08-18 11:08:53 +0100

    Address comments

--- application/src/main/kotlin/net/corda/v5/application/uniqueness/client/UniquenessCheckerClientService.kt
+++ application/src/main/kotlin/net/corda/v5/application/uniqueness/client/UniquenessCheckerClientService.kt
@@ -6,49 +6,35 @@ import java.time.Instant
 import java.util.concurrent.Future
 
 /**
- * This service is used to queue a request for uniqueness checking.
- * The request will be processed asynchronously and a [Future] will
- * be returned that can be used to check the outcome.
- *
- * This service can be injected to either flows or other services.
- *
- * The actual request processing depends on which uniqueness service
- * is invoked from the client.
- *
- * The API contains only one function that is used to queue up a request.
+ * This service is used to request uniqueness checking. The request will be processed asynchronously
+ * and a [Future] will be returned that can be used to check the outcome. This service can be injected
+ * to either flows or other services.
  */
 @DoNotImplement
 interface UniquenessCheckerClientService {
 
     /**
-     * This function is used to queue up a request for the client service,
-     * which will process it in an async manner, and return a [Future].
-     *
      * @param txId The ID of the transaction that needs processing
      *
-     * @param inputs A list of the input state refs that belongs
-     * o the given transaction, in a <TX_ID>:<STATE_NUM> format.
-     * This list might be empty if the given transaction is only
-     * doing issuance.
+     * @param inputStates A list of the input state refs that belongs to the given transaction in a
+     * <TX_ID>:<STATE_NUM> format. This list might be empty if the given transaction is only doing
+     * issuance.
      *
-     * @param references A list of the reference state refs that belongs
-     * o the given transaction, in a <TX_ID>:<STATE_NUM> format.
-     * This list might be empty.
+     * @param referenceStates A list of the reference state refs that belongs to the given transaction,
+     * in a <TX_ID>:<STATE_NUM> format. This list might be empty.
      *
-     * @param numberOfOutputStates The number of output states the
-     * given transaction has.
+     * @param numOutputStates The number of output states the given transaction has.
      *
-     * @param timeWindowLowerBound The earliest date/time from which
-     * the transaction is considered valid. This is an optional parameter.
+     * @param timeWindowLowerBound The earliest date/time from which the transaction is considered valid.
+     * This is an optional parameter.
      *
-     * @param timeWindowUpperBound The latest date/time until the transaction
-     * is considered valid.
+     * @param timeWindowUpperBound The latest date/time until the transaction is considered valid.
      */
-    fun commitRequest(
+    fun requestUniquenessCheck(
         txId: String,
-        inputs: List<String>,
-        references: List<String>,
-        numberOfOutputStates: Int,
+        inputStates: List<String>,
+        referenceStates: List<String>,
+        numOutputStates: Int,
         timeWindowLowerBound: Instant?,
         timeWindowUpperBound: Instant,
     ): Future<UniquenessCheckResponse>
