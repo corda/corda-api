9a1a6c312d04a5412b29a435be7952a9c412e760
Lajos Veres <lajos.veres@r3.com>
2022-08-18 11:54:04 +0100

    CORE-5001 make TransactionVerificationException to concrete

--- ledger/ledger-common/src/main/kotlin/net/corda/v5/ledger/common/transaction/TransactionVerificationException.kt
+++ ledger/ledger-common/src/main/kotlin/net/corda/v5/ledger/common/transaction/TransactionVerificationException.kt
@@ -4,11 +4,11 @@ import net.corda.v5.application.flows.exceptions.FlowException
 import net.corda.v5.crypto.SecureHash
 
 /**
- * Indicates that some aspect of the transaction named by [txId] violates the platform rules. The exact type of failure
- * is expressed using a subclass. TransactionVerificationException is a [FlowException] and thus when thrown inside
+ * Indicates that some aspect of the transaction named by [txId] violates some rules.
+ * TransactionVerificationException is a [FlowException] and thus when thrown inside
  * a flow, the details of the failure will be serialised, propagated to the peer and rethrown.
  *
  * @property txId the Merkle root hash (identifier) of the transaction that failed verification.
  */
-abstract class TransactionVerificationException(val txId: SecureHash, message: String, cause: Throwable?)
+class TransactionVerificationException(val txId: SecureHash, message: String, cause: Throwable?)
     : FlowException("$message, transaction: $txId", cause)
