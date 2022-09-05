b67ac880ab48837b5cfb6abc73531d69c7a88db8
Charlie Crean <charlie.crean@r3.com>
2022-09-05 14:53:46 +0100

    kdocs

--- ledger-obsolete/src/main/kotlin/net/corda/v5/ledger/obsolete/LedgerGroupParameters.kt
+++ ledger-obsolete/src/main/kotlin/net/corda/v5/ledger/obsolete/LedgerGroupParameters.kt
@@ -6,6 +6,12 @@ import net.corda.v5.membership.GroupParameters
 
 private const val NOTARIES_KEY = "corda.notary"
 
+/**
+ * A list of all available notaries in the group.
+ * This attempts to parse the available notary information from the group parameters internal property map.
+ *
+ * Note: This is due to be reimplemented which is why it is currently in the `obsolete` module.
+ */
 val GroupParameters.notaries: List<NotaryInfo>
     get() = parseList(
         NOTARIES_KEY,
