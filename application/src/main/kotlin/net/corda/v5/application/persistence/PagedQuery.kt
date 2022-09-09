022f6ac43c4cac9eb9e37e2f765082acc5f6fc28
owenstanford <owen.stanford@r3.com>
2022-09-09 15:15:35 +0100

    CORE-6655 removed unused code.

--- application/src/main/kotlin/net/corda/v5/application/persistence/PagedQuery.kt
+++ application/src/main/kotlin/net/corda/v5/application/persistence/PagedQuery.kt
@@ -1,11 +1,13 @@
 package net.corda.v5.application.persistence
 
+import net.corda.v5.base.annotations.Suspendable
+
 /**
  * Used to build a Query that supports limit and offset.
  *
  * @param R The type of the results.
  */
-interface PagedQuery<R> : Query<R> {
+interface PagedQuery<R> {
     /**
      * Set the maximum number of results to return.
      * If no limit is set, all records will be returned.
@@ -27,4 +29,14 @@ interface PagedQuery<R> : Query<R> {
      * @throws IllegalArgumentException if [offset] is negative.
      */
     fun setOffset(offset: Int): PagedQuery<R>
+
+    /**
+     * Execute the [PagedQuery]
+     *
+     * @return List of entities found. Empty list if none were found.
+     *
+     * @throws CordaPersistenceException If there is an error executing the query.
+     */
+    @Suspendable
+    fun execute(): List<R>
 }
