69c33c299bfd844545be87d28313068dc7e6e3f7
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2022-09-21 15:02:06 +0100

    Update serialization/src/main/java/net/corda/v5/serialization/MissingSerializerException.java
    Co-authored-by: Nadine Quin <103633799+nadinequinr3@users.noreply.github.com>

--- serialization/src/main/java/net/corda/v5/serialization/MissingSerializerException.java
+++ serialization/src/main/java/net/corda/v5/serialization/MissingSerializerException.java
@@ -30,7 +30,7 @@ public class MissingSerializerException extends NotSerializableException {
 
     /**
      * Constructs a MissingSerializerException with the specified message and type descriptor
-     * @param message error message describing exception
+     * @param message Error message describing exception.
      * @param typeDescriptor Type descriptor to help diagnose the exception.
      */
     public MissingSerializerException(@NotNull String message, @NotNull String typeDescriptor) {
