ef72c948acc082d2711896df419164fd46605de5
Matthew Layton <matthew.layton@live.co.uk>
2022-09-26 11:40:40 +0100

    Addressed PR comments.

--- ledger/ledger-utxo/src/test/java/net/corda/v5/ledger/utxo/AbstractMockTestHarness.java
+++ ledger/ledger-utxo/src/test/java/net/corda/v5/ledger/utxo/AbstractMockTestHarness.java
@@ -3,10 +3,8 @@ package net.corda.v5.ledger.utxo;
 import net.corda.v5.ledger.utxo.transaction.UtxoLedgerTransaction;
 import org.mockito.Mockito;
 
-import java.math.BigDecimal;
 import java.security.PublicKey;
 import java.util.Set;
-import java.util.UUID;
 
 public class AbstractMockTestHarness {
 
@@ -14,65 +12,22 @@ public class AbstractMockTestHarness {
     protected final PublicKey aliceKey = Mockito.mock(PublicKey.class);
     protected final PublicKey bobKey = Mockito.mock(PublicKey.class);
     protected final ContractState contractState = Mockito.mock(ContractState.class);
-    protected final IdentifiableState identifiableState = Mockito.mock(IdentifiableState.class);
-    protected final FungibleState<BigDecimal> fungibleState = Mockito.mock(FungibleState.class);
-    protected final IssuableState issuableState = Mockito.mock(IssuableState.class);
-    protected final BearableState bearableState = Mockito.mock(BearableState.class);
     protected final Contract contract = Mockito.mock(Contract.class);
-    protected final VerifiableCommand command = Mockito.mock(VerifiableCommand.class);
-    protected final CommandAndSignatories<VerifiableCommand> commandAndSignatories = Mockito.mock(CommandAndSignatories.class);
     protected final UtxoLedgerTransaction utxoLedgerTransaction = Mockito.mock(UtxoLedgerTransaction.class);
 
     // Mocked Data
     protected final Set<PublicKey> participants = Set.of(aliceKey, bobKey);
-    protected final UUID id = UUID.fromString("00000000-0000-4000-0000-000000000000");
-    protected final BigDecimal quantity = BigDecimal.valueOf(123.45);
 
     protected AbstractMockTestHarness() {
         initializeContractState();
-        initializeIdentifiableState();
-        initializeFungibleState();
-        initializeIssuableState();
-        initializeBearableState();
         initializeContract();
-        initializeCommand();
-        initializeCommandAndSignatories();
     }
 
     private void initializeContractState() {
         Mockito.when(contractState.getParticipants()).thenReturn(participants);
     }
 
-    private void initializeIdentifiableState() {
-        Mockito.when(identifiableState.getId()).thenReturn(id);
-        Mockito.when(identifiableState.getParticipants()).thenReturn(participants);
-    }
-
-    private void initializeFungibleState() {
-        Mockito.when(fungibleState.getQuantity()).thenReturn(quantity);
-        Mockito.when(fungibleState.getParticipants()).thenReturn(participants);
-    }
-
-    private void initializeIssuableState() {
-        Mockito.when(issuableState.getIssuer()).thenReturn(aliceKey);
-        Mockito.when(issuableState.getParticipants()).thenReturn(participants);
-    }
-
-    private void initializeBearableState() {
-        Mockito.when(bearableState.getBearer()).thenReturn(bobKey);
-        Mockito.when(bearableState.getParticipants()).thenReturn(participants);
-    }
-
     private void initializeContract() {
         Mockito.doNothing().when(contract).verify(utxoLedgerTransaction);
-    }
-
-    public void initializeCommand() {
-        Mockito.doNothing().when(command).verify(utxoLedgerTransaction, participants);
-    }
-
-    public void initializeCommandAndSignatories() {
-        Mockito.when(commandAndSignatories.getCommand()).thenReturn(command);
-        Mockito.when(commandAndSignatories.getSignatories()).thenReturn(participants);
     }
 }
