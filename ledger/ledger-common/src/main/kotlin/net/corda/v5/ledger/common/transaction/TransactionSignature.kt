93c4b926e9d6c482f598eeade43e713f7269ea53
Lajos Veres <lajos.veres@r3.com>
2022-10-05 10:49:30 +0100

    CORE-5091 Refactor net.corda.v5.application.crypto.DigitalSignatureAndMetadata into net.corda.v5.ledger.common.transaction.TransactionSignature

--- ledger/ledger-common/src/main/kotlin/net/corda/v5/ledger/common/transaction/TransactionSignature.kt
+++ ledger/ledger-common/src/main/kotlin/net/corda/v5/ledger/common/transaction/TransactionSignature.kt
@@ -1,22 +1,26 @@
-package net.corda.v5.application.crypto
+package net.corda.v5.ledger.common.transaction
 
+import net.corda.v5.application.crypto.DigitalSignatureMetadata
 import java.security.PublicKey
 import net.corda.v5.base.annotations.CordaSerializable
 import net.corda.v5.crypto.DigitalSignature
+import net.corda.v5.crypto.merkle.MerkleProof
 
 /**
  * A wrapper over the signature output accompanied by signer's public key and signature metadata.
  *
- * @property signature The signature that was applied.
+ * @property signature The output signature.
  * @property metadata Attached [DigitalSignatureMetadata] for this signature.
+ * @property batchProof The signed [MerkleProof] for batch signing cases.
  * @property by The [PublicKey] that created the signature.
  *
- * @constructor Creates a [DigitalSignatureAndMetadata].
+ * @constructor Creates a [TransactionSignature].
  */
 @CordaSerializable
-data class DigitalSignatureAndMetadata(
+data class TransactionSignature(
     val signature: DigitalSignature.WithKey,
-    val metadata: DigitalSignatureMetadata
+    val metadata: DigitalSignatureMetadata,
+    val batchProof: MerkleProof? = null
 ) {
     val by: PublicKey = signature.by
 }
