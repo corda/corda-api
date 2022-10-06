36d934f74d16d0aa216c352d197226a1e434c701
Dan Newton <dan.newton@r3.com>
2022-10-06 12:43:43 +0100

    CORE-6871 Add TODO comments tickets

--- ledger/ledger-consensual/src/main/kotlin/net/corda/v5/ledger/consensual/transaction/ConsensualSignedTransactionVerifier.kt
+++ ledger/ledger-consensual/src/main/kotlin/net/corda/v5/ledger/consensual/transaction/ConsensualSignedTransactionVerifier.kt
@@ -1,13 +1,10 @@
 package net.corda.v5.ledger.consensual.transaction
 
-import net.corda.v5.base.annotations.CordaSerializable
 import net.corda.v5.base.annotations.Suspendable
 
-@CordaSerializable
 fun interface ConsensualSignedTransactionVerifier {
 
-    // Should this return a Try.Success/Error? Or control purely through exceptions, but this means that should not
-    // be used to determine behaviour in the peer flow.
+    // TODO [CORE-7027] Document that this API should throw to fail verification
     @Suspendable
     fun verify(signedTransaction: ConsensualSignedTransaction)
 }
