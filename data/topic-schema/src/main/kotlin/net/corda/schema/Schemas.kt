45298839a1567bd31d901c4df245fe9aab0d6b77
Conal Smith <conal.smith@r3.com>
2022-10-12 22:01:49 +0100

    CORE-5075 - rename topic

--- data/topic-schema/src/main/kotlin/net/corda/schema/Schemas.kt
+++ data/topic-schema/src/main/kotlin/net/corda/schema/Schemas.kt
@@ -201,7 +201,7 @@ class Schemas {
         companion object {
             const val VIRTUAL_NODE_INFO_TOPIC = "virtual.node.info"
             const val VIRTUAL_NODE_MANAGEMENT_TOPIC = "virtual.node.management"
-            const val VIRTUAL_NODE_UPGRADE_REQUEST_TOPIC = "virtual.node.upgrade.request"
+            const val VIRTUAL_NODE_UPGRADE_REQUEST_TOPIC = "virtual.node.upgrade"
             const val VIRTUAL_NODE_CREATION_REQUEST_TOPIC = "virtual.node.creation.request"
             const val VIRTUAL_NODE_CREATION_REQUEST_RESPONSE_TOPIC = "virtual.node.creation.request.resp"
             const val CPI_INFO_TOPIC = "cpi.info"
