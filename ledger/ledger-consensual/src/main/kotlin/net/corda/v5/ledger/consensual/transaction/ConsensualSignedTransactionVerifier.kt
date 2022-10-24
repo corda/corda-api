e30d8204251c9cac84a242b1c95d79a73504294c
Dan Newton <dan.newton@r3.com>
2022-10-24 12:42:46 +0100

    CORE-7027 Make `ConsensualSignedTransactionVerifier` serializable
    Extend `Serializable` to make lambdas that implement this interface
    serializable by Kryo. Without this, errors are thrown when deserializing
    lambdas implementing `ConsensualSignedTransactionVerifier`.
    
    This improves the syntax of `receiveFinality` as we can pass a lambda
    into it rather than defining an anonymous class.

--- ledger/ledger-consensual/src/main/kotlin/net/corda/v5/ledger/consensual/transaction/ConsensualSignedTransactionVerifier.kt
+++ ledger/ledger-consensual/src/main/kotlin/net/corda/v5/ledger/consensual/transaction/ConsensualSignedTransactionVerifier.kt
@@ -1,8 +1,9 @@
 package net.corda.v5.ledger.consensual.transaction
 
 import net.corda.v5.base.annotations.Suspendable
+import java.io.Serializable
 
-fun interface ConsensualSignedTransactionVerifier {
+fun interface ConsensualSignedTransactionVerifier : Serializable {
 
     // TODO [CORE-7027] Document that this API should throw to fail verification
     @Suspendable
