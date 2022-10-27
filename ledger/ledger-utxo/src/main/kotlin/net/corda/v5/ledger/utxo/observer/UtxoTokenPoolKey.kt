dc890959745ff085afd6a8741487e50f9ff35778
owenstanford <owen.stanford@r3.com>
2022-10-27 17:49:09 +0100

    CORE-5553 minor wording change on KDoc post PR feedback.

--- ledger/ledger-utxo/src/main/kotlin/net/corda/v5/ledger/utxo/observer/UtxoTokenPoolKey.kt
+++ ledger/ledger-utxo/src/main/kotlin/net/corda/v5/ledger/utxo/observer/UtxoTokenPoolKey.kt
@@ -5,6 +5,9 @@ import net.corda.v5.crypto.SecureHash
 /**
  * The [UtxoTokenPoolKey] defines a key for a pool of similar tokens.
  *
+ * The full token pool key includes Holding ID, token type, issue, notary and symbol. The platform provides the
+ * holding ID, notary and optional token type.
+ *
  * @property tokenType The type of token within a pool. If nothing is sepecified the platform will default this to the
  * class name of the contact state this key was created from by an implementation of [UtxoLedgerTokenStateObserver]
  * @property issuerHash The [SecureHash] of the issuer of the tokens in a pool.
