2547a11b5edd72e099b061d9e816c6d34035566d
Charlie Crean <charlie.crean@r3.com>
2022-11-11 09:52:35 +0000

    add new config for group params reconcilation interval

--- data/config-schema/src/main/kotlin/net/corda/schema/configuration/ReconciliationConfig.kt
+++ data/config-schema/src/main/kotlin/net/corda/schema/configuration/ReconciliationConfig.kt
@@ -6,5 +6,6 @@ object ReconciliationConfig {
         const val RECONCILIATION_CPK_WRITE_INTERVAL_MS = "cpkWriteIntervalMs"
         const val RECONCILIATION_CPI_INFO_INTERVAL_MS = "cpiInfoIntervalMs"
         const val RECONCILIATION_CONFIG_INTERVAL_MS = "configIntervalMs"
+        const val RECONCILIATION_GROUP_PARAMS_INTERVAL_MS = "groupParamsIntervalMs"
         const val RECONCILIATION_VNODE_INFO_INTERVAL_MS ="vnodeInfoIntervalMs"
 }
