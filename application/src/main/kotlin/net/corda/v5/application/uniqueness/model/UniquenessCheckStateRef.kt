e2c2bf2ce93f0022935737167fd0ac8e5b714aa6
nkovacsx <57627796+nkovacsx@users.noreply.github.com>
2023-01-09 14:44:56 +0000

    Update application/src/main/kotlin/net/corda/v5/application/uniqueness/model/UniquenessCheckStateRef.kt
    Co-authored-by: Ramzi El-Yafi <ramzi.el-yafi@r3.com>

--- application/src/main/kotlin/net/corda/v5/application/uniqueness/model/UniquenessCheckStateRef.kt
+++ application/src/main/kotlin/net/corda/v5/application/uniqueness/model/UniquenessCheckStateRef.kt
@@ -9,9 +9,9 @@ import net.corda.v5.crypto.SecureHash
  * model and is agnostic to both the message bus API and any DB schema that may be used to persist data
  * by the backing store.
  *
- * Please note that this representation of a state ref is entirely different from the Ledger specific `StateRef` class.
- * This class represents a state ref that the uniqueness checker will process and might be different from the UTXO data
- * model.
+ * Please note that this representation of a state ref is entirely different from the Ledger specific
+ * [StateRef][net.corda.v5.ledger.utxo.StateRef] class. This class represents a state ref that the
+ * uniqueness checker will process and may differ from the UTXO ledger data model.
  */
 @CordaSerializable
 interface UniquenessCheckStateRef {
