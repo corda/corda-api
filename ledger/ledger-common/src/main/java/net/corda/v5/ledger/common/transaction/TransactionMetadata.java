d4ce346207c29a5c999a0827d26da65eef0fc21d
suhas-srivastava <suhas.srivastava@r3.com>
2023-02-14 19:15:46 +0530

    CORE-10335: Migrated code for TransactionMetadata.kt to java.

--- ledger/ledger-common/src/main/java/net/corda/v5/ledger/common/transaction/TransactionMetadata.java
+++ ledger/ledger-common/src/main/java/net/corda/v5/ledger/common/transaction/TransactionMetadata.java
@@ -1,71 +1,75 @@
-package net.corda.v5.ledger.common.transaction
+package net.corda.v5.ledger.common.transaction;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * This interface represents the metadata governing a transaction, capturing a snapshot of the system and software versions as the
  * transaction was created.
  */
-interface TransactionMetadata {
+public interface TransactionMetadata {
 
     /**
      * Gets the ledger model this transaction belongs to.
      *
      * @return The class name of the transaction implementation.
      */
-    fun getLedgerModel(): String
+    @NotNull
+    String getLedgerModel();
 
     /**
      * Gets the version of the ledger this transaction was created with.
      *
      * @return The ledger version at creation time.
      */
-    fun getLedgerVersion(): Int
+    int getLedgerVersion();
 
     /**
      * Gets the transaction subtype. This is ledger specific, check with the documentation of the ledger model you are using.
      *
      * @return The transaction subtype as string defined in the ledger model.
      */
-    fun getTransactionSubtype(): String?
+    @Nullable
+    String getTransactionSubtype();
 
     /**
      * Gets information about the CPI running on the virtual node creating the transaction.
      *
      * @return A summary of the CPI.
      */
-    fun getCpiMetadata(): CordaPackageSummary?
+    @Nullable
+    CordaPackageSummary getCpiMetadata();
 
     /**
      * Gets information about the contract CPKs governing the transaction (installed on the virtual node when the transaction was created).
      *
      * @return A list of CPK summaries.
      */
-    fun getCpkMetadata(): List<CordaPackageSummary>
+    @NotNull
+    List<CordaPackageSummary> getCpkMetadata();
 
     /**
      * Gets the number of the component groups included in the transaction.
      *
      * @return The number of component groups.
      */
-    fun getNumberOfComponentGroups(): Int
+    int getNumberOfComponentGroups();
 
     /**
      * Gets the digest settings used to calculate the transaction hashes.
      *
      * @return The digest settings as map.
      */
-    fun getDigestSettings(): Map<String, String>
+    @NotNull
+    Map<String, String> getDigestSettings();
 
     /**
      * Gets the version of the metadata JSON schema to parse this metadata entity.
      *
      * @return The schema version.
      */
-    fun getSchemaVersion(): Int
-
-    /**
-     * Gets the version of the platform at the time of the creation of the transaction.
-     *
-     * @return The platform version.
-     */
-    fun getPlatformVersion(): Int
+    int getSchemaVersion();
 }
