3f2dfa6c4f1d43f905caa3bff4245591cf0992f6
suhas-srivastava <suhas.srivastava@r3.com>
2023-02-14 20:13:24 +0530

    CORE-10335: Migrated code of TransactionSignatureService.kt to java.

--- ledger/ledger-common/src/main/java/net/corda/v5/ledger/common/transaction/TransactionSignatureService.java
+++ ledger/ledger-common/src/main/java/net/corda/v5/ledger/common/transaction/TransactionSignatureService.java
@@ -1,57 +1,62 @@
-package net.corda.v5.ledger.common.transaction
+package net.corda.v5.ledger.common.transaction;
 
-import net.corda.v5.application.crypto.DigitalSignatureAndMetadata
-import net.corda.v5.application.crypto.DigitalSignatureVerificationService
-import net.corda.v5.base.annotations.Suspendable
-import net.corda.v5.crypto.merkle.MerkleProof
-import java.security.PublicKey
+import net.corda.v5.application.crypto.DigitalSignatureAndMetadata;
+import net.corda.v5.application.crypto.DigitalSignatureVerificationService;
+import net.corda.v5.base.annotations.Suspendable;
+import net.corda.v5.crypto.merkle.MerkleProof;
+import org.jetbrains.annotations.NotNull;
+
+import java.security.PublicKey;
+import java.util.List;
 
 /**
  * TransactionSignatureService can be used to sign and verify transaction signatures.
  * It supports both single and batch signatures.
  */
 
-interface TransactionSignatureService {
+public interface TransactionSignatureService {
     /**
      * Signs a transaction id with all the available keys.
      *
      * @param transaction The transaction to be signed.
      * @param publicKeys Public keys that correspond to the private keys which should be attempted to sign with.
      *
-     * @returns Resulting signatures.
+     * @return Resulting signatures.
      *
      * @throws TransactionNoAvailableKeysException If none of the keys are available.
      */
     @Suspendable
-    fun sign(transaction: TransactionWithMetadata, publicKeys: Iterable<PublicKey>): List<DigitalSignatureAndMetadata>
+    @NotNull
+    List<DigitalSignatureAndMetadata> sign(@NotNull  TransactionWithMetadata transaction, @NotNull Iterable<PublicKey> publicKeys);
 
     /**
      * Signs a list of transactions with each the available keys.
      * It creates one batch signature for each available keys.
-     * Then returns the signatures for each transaction with a [MerkleProof] to prove that they are included in the batch.
+     * Then returns the signatures for each transaction with a {@link MerkleProof} to prove that they are included in the batch.
      *
      * @param transactions The transactions to be signed.
      * @param publicKeys Public keys that correspond to the private keys which should be attempted to sign with.
      *
-     * @returns List of signatures for each supplied transaction.
+     * @return {@link List} of signatures for each supplied transaction.
      *          The outer list will always be of the same size and in the same order as the supplied [transactions].
      *
      * @throws TransactionNoAvailableKeysException If none of the keys are available.
      */
     @Suspendable
-    fun signBatch(
-        transactions: List<TransactionWithMetadata>,
-        publicKeys: Iterable<PublicKey>
-    ): List<List<DigitalSignatureAndMetadata>>
+    @NotNull
+    List<List<DigitalSignatureAndMetadata>> signBatch(
+            @NotNull List<TransactionWithMetadata> transactions,
+            @NotNull Iterable<PublicKey> publicKeys
+    );
 
     /**
      * Verifies a signature against a transaction.
      * The underlying verification service signals the verification failures with different exceptions.
-     * [DigitalSignatureVerificationService]
+     * {@link DigitalSignatureVerificationService}
      *
      * @param transaction The original transaction.
      * @param signatureWithMetadata The signature to be verified.
      *
      */
-    fun verifySignature(transaction: TransactionWithMetadata, signatureWithMetadata: DigitalSignatureAndMetadata)
+    void verifySignature(@NotNull TransactionWithMetadata transaction, @NotNull DigitalSignatureAndMetadata signatureWithMetadata);
 }
