e7f5fd2d2b7f3ed46d2e02c263456709ab788cfe
Alexander Cramb <alex.cramb@r3.com>
2023-02-21 12:31:43 +0000

    Create InteropService class and move new API

---
+++ application/src/main/java/net/corda/v5/application/interop/InteropService.java
@@ -0,0 +1,16 @@
+package net.corda.v5.application.interop;
+
+import net.corda.v5.base.annotations.DoNotImplement;
+import net.corda.v5.base.types.MemberX500Name;
+
+@DoNotImplement
+public interface InteropService {
+    /**
+     * Call an Interop facade.
+     *
+     * @param memberName The MemberX500name of the alter-ego.
+     * @param facadeName The name of the facade to invoke.
+     * @param methodName The name of the function within the facade to invoke.
+     */
+    void callFacade(MemberX500Name memberName, String facadeName, String methodName);
+}
