f5c5a0e1ec6376dafd3b04c39e093ed858de0928
Alexander Cramb <alex.cramb@r3.com>
2023-02-21 12:57:00 +0000

    Add suspendable annotation to new API method

--- application/src/main/java/net/corda/v5/application/interop/InteropService.java
+++ application/src/main/java/net/corda/v5/application/interop/InteropService.java
@@ -1,6 +1,7 @@
 package net.corda.v5.application.interop;
 
 import net.corda.v5.base.annotations.DoNotImplement;
+import net.corda.v5.base.annotations.Suspendable;
 import net.corda.v5.base.types.MemberX500Name;
 
 @DoNotImplement
@@ -12,5 +13,6 @@ public interface InteropService {
      * @param facadeName The name of the facade to invoke.
      * @param methodName The name of the function within the facade to invoke.
      */
+    @Suspendable
     void callFacade(MemberX500Name memberName, String facadeName, String methodName);
 }
