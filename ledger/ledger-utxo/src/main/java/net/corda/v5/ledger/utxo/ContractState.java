339a930a4ec77d6b179a1241d0e053240e944e1a
Matthew Layton <matthew.layton@live.co.uk>
2023-02-27 09:06:32 +0000

    Added `VisibilityChecker`.

--- ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/ContractState.java
+++ ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/ContractState.java
@@ -1,12 +1,10 @@
 package net.corda.v5.ledger.utxo;
 
 import net.corda.v5.base.annotations.CordaSerializable;
-import net.corda.v5.ledger.utxo.transaction.UtxoLedgerTransaction;
 import org.jetbrains.annotations.NotNull;
 
 import java.security.PublicKey;
 import java.util.List;
-import java.util.stream.StreamSupport;
 
 /**
  * Defines a contract state.
@@ -28,19 +26,4 @@ public interface ContractState {
      */
     @NotNull
     List<PublicKey> getParticipants();
-
-    /**
-     * Determines to whom the current state is visible, given the transaction in which it's created, and the current node's public keys.
-     * <p>
-     * The default implementation behaves such that the current state is visible to its participants only.
-     *
-     * @param transaction The {@link UtxoLedgerTransaction} in which the current state is created.
-     * @param myKeys      The collection of public keys owned by the current node.
-     * @return Returns true if the current state is visible, given the transaction in which it's created, and the current node's public keys; otherwise, false.
-     */
-    default boolean isVisible(@NotNull UtxoLedgerTransaction transaction, @NotNull Iterable<PublicKey> myKeys) {
-        return StreamSupport
-                .stream(myKeys.spliterator(), false)
-                .anyMatch(key -> getParticipants().contains(key));
-    }
 }
