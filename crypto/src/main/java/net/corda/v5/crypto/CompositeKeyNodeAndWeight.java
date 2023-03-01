210f059b137928697c567032bb6edc7ffd85f432
Dickon Reed <dickon.reed@r3.com>
2023-03-01 16:51:33 +0000

    CORE-10336: add data methods for CompositeKeyNodeAndWeight

--- crypto/src/main/java/net/corda/v5/crypto/CompositeKeyNodeAndWeight.java
+++ crypto/src/main/java/net/corda/v5/crypto/CompositeKeyNodeAndWeight.java
@@ -1,4 +1,5 @@
 package net.corda.v5.crypto;
+
 import org.jetbrains.annotations.NotNull;
 
 import java.security.PublicKey;
@@ -6,24 +7,54 @@ import java.security.PublicKey;
 
 /**
  * A simple data class for passing keys and weights into CompositeKeyGenerator
- *
- * @param node A public key
- * @param weight The weight for that key, must be greater than zero.
  */
 public final class CompositeKeyNodeAndWeight {
+    /**
+     * Construct a key
+     *
+     * @param node   A public key
+     * @param weight The weight for that key, must be greater than zero.
+     */
+    public CompositeKeyNodeAndWeight(@NotNull PublicKey node, int weight) {
+        super();
+        this.node = node;
+        this.weight = weight;
+        if (this.weight <= 0)
+            throw new IllegalArgumentException("A non-positive weight was detected. Member info: " + this);
+    }
+
     @NotNull
     private final PublicKey node;
     private final int weight;
-    
-    public CompositeKeyNodeAndWeight(@NotNull PublicKey node, @NotNull int weight) {
-        super();
-        this.node = node;
-        this.weight=weight;
-        if (this.weight <= 0) throw new IllegalArgumentException("A non-positive weight was detected. Member info: " + this);
+
+
+    public CompositeKeyNodeAndWeight(@NotNull PublicKey node) {
+        this(node, 1);
     }
 
-    public CompositeKeyNodeAndWeight(@NotNull PublicKey node) { this(node, 1); }
-    
-    @NotNull public final PublicKey getNode() { return this.node; }
-    public final int getWeight() { return this.weight; }
+    @NotNull
+    public PublicKey getNode() {
+        return this.node;
+    }
+
+    public int getWeight() {
+        return this.weight;
+    }
+
+    @NotNull
+    public String toString() {
+        return String.format("[%s, %d]", this.node, weight);
+    }
+
+    public int hashCode() {
+        return node.hashCode() + 31 * weight;
+    }
+
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (!(other instanceof CompositeKeyNodeAndWeight)) return false;
+        CompositeKeyNodeAndWeight otherKey = (CompositeKeyNodeAndWeight) other;
+        if (!otherKey.node.equals(node)) return false;
+        return otherKey.weight == weight;
+    }
 }
--- crypto/src/main/java/net/corda/v5/crypto/CordaOID.java
+++ crypto/src/main/java/net/corda/v5/crypto/CordaOID.java
@@ -14,24 +14,24 @@ public final class CordaOID {
     public static final String OID_R3_ROOT = "1.3.6.1.4.1.50530";
 
     /** OIDs issued for the Corda platform. */
-    public static final String OID_CORDA_PLATFORM = "1.3.6.1.4.1.50530.1";
+    public static final String OID_CORDA_PLATFORM = OID_R3_ROOT + ".1";
 
     /**
      * Identifier for the X.509 certificate extension specifying the Corda role. See
      * https://r3-cev.atlassian.net/wiki/spaces/AWG/pages/156860572/Certificate+identity+type+extension for details.
      */
     
-    public static final String OID_X509_EXTENSION_CORDA_ROLE = "1.3.6.1.4.1.50530.1.1";
+    public static final String OID_X509_EXTENSION_CORDA_ROLE = OID_CORDA_PLATFORM + ".1";
 
     /** OID for AliasPrivateKey. */
 
-    public static final String OID_ALIAS_PRIVATE_KEY = "1.3.6.1.4.1.50530.1.2";
+    public static final String OID_ALIAS_PRIVATE_KEY = OID_CORDA_PLATFORM + ".2";
 
     /** OID for CompositeKey. */
 
-    public static final String OID_COMPOSITE_KEY = "1.3.6.1.4.1.50530.1.3";
+    public static final String OID_COMPOSITE_KEY = OID_CORDA_PLATFORM + ".3";
 
     /** OID for CompositeSignature. */
 
-    public static final String OID_COMPOSITE_SIGNATURE = "1.3.6.1.4.1.50530.1.4";
+    public static final String OID_COMPOSITE_SIGNATURE = OID_CORDA_PLATFORM + ".4";
 }
