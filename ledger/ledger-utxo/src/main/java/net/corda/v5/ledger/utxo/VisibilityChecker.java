a48bc264450ab35230dde2f942a811dfa24b5999
Matthew Layton <matthew.layton@live.co.uk>
2023-03-02 18:13:33 +0000

    Refactored `isRelevant` to `isVisible` which now relies on a `VisibilityChecker`.

---
+++ ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/VisibilityChecker.java
@@ -0,0 +1,19 @@
+package net.corda.v5.ledger.utxo;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.security.PublicKey;
+
+/**
+ * Provides functionality to determine state visibility.
+ */
+public interface VisibilityChecker {
+
+    /**
+     * Determines whether any of the specified keys belong to the current node.
+     *
+     * @param keys The keys for which to determine belong to the current node.
+     * @return Returns true if any of the specified keys belong to the current node; otherwise, false.
+     */
+    boolean containsMySigningKeys(@NotNull Iterable<PublicKey> keys);
+}
