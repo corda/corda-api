fb95aba76540ca06d67d26fb495a8ad3c15db249
Dickon Reed <dickon.reed@r3.com>
2023-03-02 08:22:28 +0000

    CORE-10336: move constructor to start

--- crypto/src/main/java/net/corda/v5/crypto/ParameterizedSignatureSpec.java
+++ crypto/src/main/java/net/corda/v5/crypto/ParameterizedSignatureSpec.java
@@ -1,37 +1,46 @@
 package net.corda.v5.crypto;
 
 import java.security.spec.AlgorithmParameterSpec;
+
 import org.jetbrains.annotations.NotNull;
 
 /**
  * This class is used to define a digital signature scheme which has the additional algorithm parameters,
  * such as RSASSA-PSS
- *
- * @param signatureName A signature-scheme name as required to create [java.security.Signature]
- * objects (e.g. "SHA256withECDSA")
- * @param params Signature parameters. For example, if using RSASSA-PSS, to avoid
- * using the default SHA1, you must specify the signature parameters explicitly.
- *
- * When used for signing the [signatureName] must match the corresponding key scheme, e.g. you cannot use
- * "SHA256withECDSA" with "RSA" keys.
  */
 public final class ParameterizedSignatureSpec extends SignatureSpec {
-    @NotNull private final AlgorithmParameterSpec params;
 
     /**
-     * Converts a [ParameterizedSignatureSpec] object to a string representation.
+     * Construct a paremeterized signature spec.
+     *
+     * @param signatureName A signature-scheme name as required to create [java.security.Signature]
+     *                      objects (e.g. "SHA256withECDSA")
+     * @param params        Signature parameters. For example, if using RSASSA-PSS, to avoid
+     *                      using the default SHA1, you must specify the signature parameters explicitly.
+     *                      <p>
+     *                      When used for signing the [signatureName] must match the corresponding key scheme, e.g. you cannot use
+     *                      "SHA256withECDSA" with "RSA" keys.
      */
-    @NotNull public String toString() {
-        return this.getSignatureName() + ':' + this.params.getClass().getSimpleName();
-    }
-
-    @NotNull
-    public final AlgorithmParameterSpec getParams() {
-        return this.params;
-    }
-
     public ParameterizedSignatureSpec(@NotNull String signatureName, @NotNull AlgorithmParameterSpec params) {
         super(signatureName);
         this.params = params;
     }
+
+    @NotNull
+    private final AlgorithmParameterSpec params;
+
+    /**
+     * Converts a [ParameterizedSignatureSpec] object to a string representation.
+     */
+    @NotNull
+    public String toString() {
+        return this.getSignatureName() + ':' + this.params.getClass().getSimpleName();
+    }
+
+    @NotNull
+    public AlgorithmParameterSpec getParams() {
+        return this.params;
+    }
+
+
 }
