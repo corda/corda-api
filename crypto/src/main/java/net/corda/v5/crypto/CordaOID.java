c71ddc84f37ee61426b2480638cce2df5c08ed84
Dickon Reed <dickon.reed@r3.com>
2023-03-02 14:14:43 +0000

    CORE-10336: use class constant pattern more consistently

--- crypto/src/main/java/net/corda/v5/crypto/CordaOID.java
+++ crypto/src/main/java/net/corda/v5/crypto/CordaOID.java
@@ -1,5 +1,7 @@
 package net.corda.v5.crypto;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * OIDs used for the Corda platform. All entries MUST be defined in this file only and they MUST NOT be removed.
  * If an OID is incorrectly assigned, it should be marked deprecated and NEVER be reused again.
@@ -13,11 +15,13 @@ public final class CordaOID {
     /**
      * Assigned to R3, see http://www.oid-info.com/cgi-bin/display?oid=1.3.6.1.4.1.50530&action=display
      */
+    @NotNull
     public static final String OID_R3_ROOT = "1.3.6.1.4.1.50530";
 
     /**
      * OIDs issued for the Corda platform.
      */
+    @NotNull
     public static final String OID_CORDA_PLATFORM = OID_R3_ROOT + ".1";
 
     /**
@@ -25,23 +29,27 @@ public final class CordaOID {
      * https://r3-cev.atlassian.net/wiki/spaces/AWG/pages/156860572/Certificate+identity+type+extension for details.
      */
 
+    @NotNull
     public static final String OID_X509_EXTENSION_CORDA_ROLE = OID_CORDA_PLATFORM + ".1";
 
     /**
      * OID for AliasPrivateKey.
      */
 
+    @NotNull
     public static final String OID_ALIAS_PRIVATE_KEY = OID_CORDA_PLATFORM + ".2";
 
     /**
      * OID for CompositeKey.
      */
 
+    @NotNull
     public static final String OID_COMPOSITE_KEY = OID_CORDA_PLATFORM + ".3";
 
     /**
      * OID for CompositeSignature.
      */
 
+    @NotNull
     public static final String OID_COMPOSITE_SIGNATURE = OID_CORDA_PLATFORM + ".4";
 }
--- crypto/src/main/java/net/corda/v5/crypto/MessageAuthenticationCode.java
+++ crypto/src/main/java/net/corda/v5/crypto/MessageAuthenticationCode.java
@@ -1,14 +1,23 @@
 package net.corda.v5.crypto;
 
-public interface MessageAuthenticationCode {
+import org.jetbrains.annotations.NotNull;
 
-    /**
+public final class MessageAuthenticationCode {
+
+    private MessageAuthenticationCode() {
+        // Not constructed; this class only exists to make the string constants available 
+    }
+    
+    /*
      * Constant specifying the HMAC SHA-256 algorithm.
      */
-    String HMAC_SHA256_ALGORITHM = "HmacSHA256";
+    @NotNull
+    public static final String HMAC_SHA256_ALGORITHM = "HmacSHA256";
 
-    /**
+    /*
      * Constant specifying the HMAC SHA-512 algorithm.
      */
-    String HMAC_SHA512_ALGORITHM = "HmacSHA512";
+    @NotNull
+    public final static String HMAC_SHA512_ALGORITHM = "HmacSHA512";
+    
 }
