55057582d192ef42f34ab194190bbf08be2eab88
Matthew Layton <matthew.layton@live.co.uk>
2023-03-09 14:12:48 +0000

    Added @Suspendable annotation.

--- ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/VisibilityChecker.java
+++ ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/VisibilityChecker.java
@@ -1,6 +1,7 @@
 package net.corda.v5.ledger.utxo;
 
 import net.corda.v5.base.annotations.DoNotImplement;
+import net.corda.v5.base.annotations.Suspendable;
 import org.jetbrains.annotations.NotNull;
 
 import java.security.PublicKey;
@@ -17,5 +18,6 @@ public interface VisibilityChecker {
      * @param keys The keys for which to determine belong to the current node.
      * @return Returns true if any of the specified keys belong to the current node; otherwise, false.
      */
+    @Suspendable
     boolean containsMySigningKeys(@NotNull Iterable<PublicKey> keys);
 }
