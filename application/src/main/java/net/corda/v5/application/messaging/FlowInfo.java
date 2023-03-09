5080ef3999c24cf2ed86deb2a31a5df47ccb70ff
LWogan <lorcan.wogan@r3.com>
2023-03-09 09:41:43 +0000

    CORE-6564 add corda serializable

--- application/src/main/java/net/corda/v5/application/messaging/FlowInfo.java
+++ application/src/main/java/net/corda/v5/application/messaging/FlowInfo.java
@@ -1,5 +1,8 @@
 package net.corda.v5.application.messaging;
 
+import net.corda.v5.base.annotations.CordaSerializable;
+
+@CordaSerializable
 public interface FlowInfo {
     /**
      * @return The protocol name the flow is running.
