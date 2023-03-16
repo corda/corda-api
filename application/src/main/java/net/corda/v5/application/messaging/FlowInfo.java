b26237012bccf6bf1d9efe8c672acc9e5d53ee73
LWogan <lorcan.wogan@r3.com>
2023-03-16 12:21:51 +0000

    CORE-6564 make version nullable

--- application/src/main/java/net/corda/v5/application/messaging/FlowInfo.java
+++ application/src/main/java/net/corda/v5/application/messaging/FlowInfo.java
@@ -1,16 +1,18 @@
 package net.corda.v5.application.messaging;
 
 import net.corda.v5.base.annotations.CordaSerializable;
+import org.jetbrains.annotations.NotNull;
 
 @CordaSerializable
 public interface FlowInfo {
     /**
      * @return The protocol name the flow is running.
      */
+    @NotNull
     String protocol();
 
     /**
-     * @return The protocol version the flow is running.
+     * @return The protocol version the flow is running. Can be null if no version associated
      */
     Integer protocolVersion();
 }
