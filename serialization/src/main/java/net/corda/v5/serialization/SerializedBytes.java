f1529310137de01721f97c0050d62db971453b9e
Anna Paskova <anna.paskova@r3.com>
2023-03-21 08:53:27 +0000

    Add DoNotImplement annotation

--- serialization/src/main/java/net/corda/v5/serialization/SerializedBytes.java
+++ serialization/src/main/java/net/corda/v5/serialization/SerializedBytes.java
@@ -1,12 +1,14 @@
 package net.corda.v5.serialization;
 
 import net.corda.v5.base.annotations.CordaSerializable;
+import net.corda.v5.base.annotations.DoNotImplement;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * A type safe wrapper around a byte array that contains a serialised object.
  */
 @SuppressWarnings("unused")
+@DoNotImplement
 @CordaSerializable
 public interface SerializedBytes<T> {
 
