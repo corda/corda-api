a37ee5ae60f50488a2589e1f5fdf16a7097b839d
nkovacsx <nandor.kovacs@r3.com>
2023-03-23 09:49:01 +0000

    more JDocs

--- ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/query/VaultNamedParameterizedQuery.java
+++ ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/query/VaultNamedParameterizedQuery.java
@@ -5,8 +5,18 @@ import org.jetbrains.annotations.NotNull;
 
 import java.time.Instant;
 
+/**
+ * Representation of a named ledger query.
+ *
+ * @param <R> The type of results this query will fetch.
+ */
 public interface VaultNamedParameterizedQuery<R> extends ParameterizedQuery<R> {
 
+    /**
+     * Sets the timestamp limit for the query. This will influence which results are returned.
+     * @param timestampLimit The timestamp limit the query should enforce.
+     * @return A {@link VaultNamedParameterizedQuery} object with the timestamp limit set.
+     */
     @NotNull
     VaultNamedParameterizedQuery<R> setCreatedTimestampLimit(@NotNull Instant timestampLimit);
 }
