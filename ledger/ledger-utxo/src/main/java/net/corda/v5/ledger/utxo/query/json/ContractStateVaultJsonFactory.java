48aa8852f250371668aa2c351f8b5aadc9fc12d5
nkovacsx <nandor.kovacs@r3.com>
2023-04-04 09:22:14 +0100

    address comments

--- ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/query/json/ContractStateVaultJsonFactory.java
+++ ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/query/json/ContractStateVaultJsonFactory.java
@@ -2,16 +2,26 @@ package net.corda.v5.ledger.utxo.query.json;
 
 import net.corda.v5.application.marshalling.JsonMarshallingService;
 import net.corda.v5.ledger.utxo.ContractState;
+import net.corda.v5.ledger.utxo.UtxoLedgerService;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * The interface that needs to be implemented in order to create a JSON representation of a state type. Implementing
- * this interface will define how a state can be represented as a JSON string.
+ * Implement a {@link ContractStateVaultJsonFactory} to create a JSON representation of a state to store alongside
+ * the state in the vault. This JSON representation can be used to query states from flows using
+ * {@link UtxoLedgerService#query}.
+ * <p>
+ * Classes that implement {@link ContractStateVaultJsonFactory} will be executed in a hierarchical way. That means
+ * every factory that belongs to a given state or that given state's ancestors will be executed and the end result
+ * will be a combined JSON. The combined JSON will be keyed by the state type specified by the factory.
+ * <p>
+ * Please note that only one factory can be registered for a state type.
  * <p>
  * Example usage:
  * <ul>
  * <li>Kotlin:<pre>{@code
  * class TestUtxoStateVaultJsonFactory: ContractStateVaultJsonFactory<TestUtxoState> {
+ *
+ *     private data class TestUtxoStatePojo(val testField: String)
  *
  *     override val stateType: Class<TestUtxoState> = TestUtxoState::class.java
  *
@@ -20,7 +30,6 @@ import org.jetbrains.annotations.NotNull;
  *     }
  * }
  *
- * data class TestUtxoStatePojo(val testField: String)
  * }</pre></li>
  * <li>Java:<pre>{@code
  * public class TestUtxoStateVaultJsonFactory implements ContractStateVaultJsonFactory<TestUtxoState> {
@@ -30,6 +39,10 @@ import org.jetbrains.annotations.NotNull;
  *
  *         TestUtxoStatePojo(String testField) {
  *             this.testField = testField;
+ *         }
+ *
+ *         String getTestField() {
+ *             return this.testField;
  *         }
  *     }
  *
@@ -63,5 +76,5 @@ public interface ContractStateVaultJsonFactory<T extends ContractState> {
      *
      * @return The JSON representation as a String.
      */
-    @NotNull String append(@NotNull T state, @NotNull JsonMarshallingService jsonMarshallingService);
+    @NotNull String create(@NotNull T state, @NotNull JsonMarshallingService jsonMarshallingService);
 }
