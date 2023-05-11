41b528748c710583de3967e3288b044a882258ef
Filipe Oliveira <filipe.oliveira@r3.com>
2023-05-11 11:45:53 +0100

    Added comments to `TokenBalance`.

--- ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/token/selection/TokenBalance.java
+++ ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/token/selection/TokenBalance.java
@@ -4,12 +4,29 @@ import net.corda.v5.base.annotations.DoNotImplement;
 import org.jetbrains.annotations.NotNull;
 import java.math.BigDecimal;
 
+/**
+ * Defines a balance of a set of tokens returned by a call to {@link TokenSelection#queryBalance(TokenBalanceCriteria)}.
+ * <p>
+ * The balance is defined by a pair of values. One of the values represents the available balance. While the other one
+ * represents the total balance, i.e., it is the available balance plus the tokens that have been claimed
+ * but which have not been spent yet.
+ */
 @DoNotImplement
 public interface TokenBalance {
 
+    /**
+     * Gets the available balance.
+     *
+     * @return Returns a value that represent the available balance.
+     */
     @NotNull
     BigDecimal getBalance();
 
+    /**
+     * Gets the total balance.
+     *
+     * @return Returns a value that represent the total balance.
+     */
     @NotNull
     BigDecimal getBalanceIncludingClaimedTokens();
 }
