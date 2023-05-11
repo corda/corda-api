ef9e1fafef2795300a07393c3ebdd8c053b6ce5c
Filipe Oliveira <filipe.oliveira@r3.com>
2023-05-11 11:24:39 +0100

    Updated the `TokenBalanceCriteria`

--- ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/token/selection/TokenBalanceCriteria.java
+++ ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/token/selection/TokenBalanceCriteria.java
@@ -1,29 +1,91 @@
 package net.corda.v5.ledger.utxo.token.selection;
 
 import net.corda.v5.base.types.MemberX500Name;
-import net.corda.v5.ledger.utxo.observer.UtxoTokenPoolKey;
+import net.corda.v5.crypto.SecureHash;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.text.MessageFormat;
 import java.util.Objects;
 
-public class TokenBalanceCriteria {
+/**
+ * Represents a description of the selection criteria for a token selection query using the {@link TokenSelection} API.
+ */
+public final class TokenBalanceCriteria {
 
-    private UtxoTokenPoolKey utxoTokenPoolKey;
-    private MemberX500Name notaryX500Name;
+    /**
+     * The type of tokens to be selected.
+     */
+    @NotNull
+    private final String tokenType;
 
+    /**
+     * The {@link SecureHash} of the issuer of tokens to be selected.
+     */
+    @NotNull
+    private final SecureHash issuerHash;
+
+    /**
+     * The {@link MemberX500Name} of the notary of the tokens to be selected.
+     */
+    @NotNull
+    private final MemberX500Name notaryX500Name;
+
+    /**
+     * The symbol of the notary of tokens to be selected.
+     */
+    @NotNull
+    private final String symbol;
+
+    /**
+     * An optional regular expression to match against the {@link ClaimedToken#getTag()} field, or null to match all tags.
+     */
+    @Nullable
+    private String tagRegex;
+
+    /**
+     * An optional owner {@link SecureHash} of the tokens to be selected, or null to match all owners.
+     */
+    @Nullable
+    private SecureHash ownerHash;
+
+    /**
+     * Creates a new instance of the {@link TokenBalanceCriteria} class.
+     *
+     * @param tokenType      The type of tokens to be selected.
+     * @param issuerHash     The {@link SecureHash} of the issuer of tokens to be selected.
+     * @param notaryX500Name The {@link MemberX500Name} of the notary of the tokens to be selected.
+     * @param symbol         The symbol of the notary of tokens to be selected.
+     */
     public TokenBalanceCriteria(
-            @NotNull final UtxoTokenPoolKey utxoTokenPoolKey,
-            @NotNull final MemberX500Name notaryX500Name
-    ) {
-        this.utxoTokenPoolKey = utxoTokenPoolKey;
+            @NotNull final String tokenType,
+            @NotNull final SecureHash issuerHash,
+            @NotNull final MemberX500Name notaryX500Name,
+            @NotNull final String symbol) {
+        this.tokenType = tokenType;
+        this.issuerHash = issuerHash;
         this.notaryX500Name = notaryX500Name;
+        this.symbol = symbol;
     }
 
+    /**
+     * Gets the type of tokens to be selected.
+     *
+     * @return Returns the type of tokens to be selected.
+     */
     @NotNull
-    public UtxoTokenPoolKey getUtxoTokenPoolKey() {
-        return utxoTokenPoolKey;
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    /**
+     * Gets the {@link SecureHash} of the issuer of tokens to be selected.
+     *
+     * @return Returns the {@link SecureHash} of the issuer of tokens to be selected.
+     */
+    @NotNull
+    public SecureHash getIssuerHash() {
+        return issuerHash;
     }
 
     /**
@@ -36,15 +98,49 @@ public class TokenBalanceCriteria {
         return notaryX500Name;
     }
 
-    public void setUtxoTokenPoolKey(@Nullable final UtxoTokenPoolKey utxoTokenPoolKey) {
-        this.utxoTokenPoolKey = utxoTokenPoolKey;
-    }
-
+    /**
+     * Gets the symbol of the notary of tokens to be selected.
+     *
+     * @return Returns the symbol of the notary of tokens to be selected.
+     */
     @NotNull
-    public void setNotaryX500Name(MemberX500Name notaryX500Name) {
-        this.notaryX500Name = notaryX500Name;
+    public String getSymbol() {
+        return symbol;
     }
 
+    /**
+     * Gets an optional regular expression to match against the {@link ClaimedToken#getTag()} field, or null to match all tags.
+     *
+     * @return Returns an optional regular expression to match against the ClaimedToken.tag field, or null to match all tags.
+     */
+    @Nullable
+    public String getTagRegex() {
+        return tagRegex;
+    }
+
+    /**
+     * Sets an optional regular expression to match against the {@link ClaimedToken#getTag()} field, or null to match all tags.
+     */
+    public void setTagRegex(@Nullable final String tagRegex) {
+        this.tagRegex = tagRegex;
+    }
+
+    /**
+     * Gets an optional owner {@link SecureHash} of the tokens to be selected, or null to match all owners.
+     *
+     * @return Returns an optional owner {@link SecureHash} of the tokens to be selected, or null to match all owners.
+     */
+    @Nullable
+    public SecureHash getOwnerHash() {
+        return ownerHash;
+    }
+
+    /**
+     * Sets an optional owner {@link SecureHash} of the tokens to be selected, or null to match all owners.
+     */
+    public void setOwnerHash(@Nullable final SecureHash ownerHash) {
+        this.ownerHash = ownerHash;
+    }
 
     /**
      * Determines whether the specified object is equal to the current object.
@@ -64,8 +160,12 @@ public class TokenBalanceCriteria {
      * @return Returns true if the specified Party is equal to the current object; otherwise, false.
      */
     public boolean equals(@NotNull final TokenBalanceCriteria other) {
-        return Objects.equals(other.utxoTokenPoolKey, utxoTokenPoolKey)
-                && Objects.equals(other.notaryX500Name, notaryX500Name);
+        return Objects.equals(other.tokenType, tokenType)
+                && Objects.equals(other.issuerHash, issuerHash)
+                && Objects.equals(other.notaryX500Name, notaryX500Name)
+                && Objects.equals(other.symbol, symbol)
+                && Objects.equals(other.tagRegex, tagRegex)
+                && Objects.equals(other.ownerHash, ownerHash);
     }
 
     /**
@@ -75,7 +175,7 @@ public class TokenBalanceCriteria {
      */
     @Override
     public int hashCode() {
-        return Objects.hash(utxoTokenPoolKey, notaryX500Name);
+        return Objects.hash(tokenType, issuerHash, notaryX500Name, symbol, tagRegex, ownerHash);
     }
 
     /**
@@ -86,8 +186,8 @@ public class TokenBalanceCriteria {
     @Override
     public String toString() {
         return MessageFormat.format(
-                "TokenBalanceCriteria(utxoTokenPoolKey={0}, notaryX500Name={1})",
-                utxoTokenPoolKey, notaryX500Name
+                "TokenBalanceCriteria(tokenType={0}, issuerHash={1}, notaryX500Name={2}, symbol={3}, tagRegex={4}, ownerHash={5})",
+                tokenType, issuerHash, notaryX500Name, symbol, tagRegex, ownerHash
         );
     }
 }
