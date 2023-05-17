3cbafb66a64ce7d28bf34ae61e3a70d9632f40e4
Julia Filipczak <66328266+julia-filipczak@users.noreply.github.com>
2023-05-17 12:42:04 +0100

    Update application/src/main/java/net/corda/v5/application/interop/parameters/TypedParameter.java
    Co-authored-by: Anna Kaludzinska <77064094+AnnaK-R3@users.noreply.github.com>

--- application/src/main/java/net/corda/v5/application/interop/parameters/TypedParameter.java
+++ application/src/main/java/net/corda/v5/application/interop/parameters/TypedParameter.java
@@ -14,7 +14,7 @@ public interface TypedParameter<T> {
 
     /**
      * @return The {@link ParameterType} of the parameter which enables calling getExpectedType() utility
-     * to get the primitive type from the ParameterTypeLabel enum, e.g. {@link ParameterTypeLabel}.STRING.
+     * to get the primitive type from the ParameterTypeLabel enum, for example, {@link ParameterTypeLabel}.STRING.
      */
     @NotNull
     ParameterType<T> getType();
