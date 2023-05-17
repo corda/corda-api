f451c88f0a524415d118390b84315c3a837b9ec5
Julia Filipczak <66328266+julia-filipczak@users.noreply.github.com>
2023-05-17 12:41:24 +0100

    Update application/src/main/java/net/corda/v5/application/interop/parameters/TypeQualifier.java
    Co-authored-by: Anna Kaludzinska <77064094+AnnaK-R3@users.noreply.github.com>

--- application/src/main/java/net/corda/v5/application/interop/parameters/TypeQualifier.java
+++ application/src/main/java/net/corda/v5/application/interop/parameters/TypeQualifier.java
@@ -19,7 +19,7 @@ public class TypeQualifier {
      * A {@link TypeQualifier} qualifies a {@link ParameterType} with a versioned identity, which may be linked to a schema
      * or validation rules for that type.
      * @param owner The owner of the type, e.g. "org.corda".
-     * @param name The name of the type, e.g. "platform/tokens/Amount".
+     * @param name The name of the type, for example, "platform/tokens/Amount".
      * @param version The version of the type, e.g. "1.0".
      */
     public TypeQualifier(@NotNull String owner, @NotNull List<String> name, @NotNull String version) {
