f93aae24f91334073a3c2ca191cfb361a1cf6320
jmacmahonr3 <james.macmahon@r3.com>
2023-05-29 17:31:39 +0100

    Missing annotations

--- application/src/main/java/net/corda/v5/application/interop/InteropIdentityLookUp.java
+++ application/src/main/java/net/corda/v5/application/interop/InteropIdentityLookUp.java
@@ -2,7 +2,10 @@ package net.corda.v5.application.interop;
 
 import net.corda.v5.application.interop.facade.FacadeId;
 import net.corda.v5.base.annotations.DoNotImplement;
+import net.corda.v5.base.annotations.Suspendable;
 import net.corda.v5.interop.InterOpIdentityInfo;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.List;
 
 /**
@@ -13,15 +16,17 @@ import java.util.List;
 public interface InteropIdentityLookUp {
 
     /**
-     * @param x500Name The x500Name of alias identity.
-     * @param hostNetwork The host network of alias identity.
      * @return The {@link InterOpIdentityInfo} representing the alias member info for given alias x500name and host network.
      */
+    @Suspendable
+    @NotNull
     InterOpIdentityInfo lookup(String applicationName);
 
     /**
      * @param facadeId The facadeId of the facade.
      * @return The {@link List< InterOpIdentityInfo >} representing the alias member info for given facadeId.
      */
+    @Suspendable
+    @NotNull
     List<InterOpIdentityInfo> lookup(FacadeId facadeId);
 }
