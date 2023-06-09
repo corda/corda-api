095497f3b881496e1a0cc0637ce836c50beb3be2
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2023-06-09 17:48:23 +0100

    CORE-11037 Add constants for datasource properties config keys (#1145)
    * Adds constants for config keys for the following datasource properties:
    
      * `idleTimeout`
      * `maxLifetime`
      * `keepaliveTime`
      * `validationTimeout`

--- data/config-schema/src/main/java/net/corda/schema/configuration/DatabaseConfig.java
+++ data/config-schema/src/main/java/net/corda/schema/configuration/DatabaseConfig.java
@@ -11,4 +11,8 @@ public final class DatabaseConfig {
     public static final String JDBC_URL = "database.jdbc.url";
     public static final String DB_POOL_MAX_SIZE = "database.pool.max_size";
     public static final String DB_POOL_MIN_SIZE = "database.pool.min_size";
+    public static final String DB_POOL_IDLE_TIMEOUT = "database.pool.idleTimeout";
+    public static final String DB_POOL_MAX_LIFETIME = "database.pool.maxLifetime";
+    public static final String DB_POOL_KEEPALIVE_TIME = "database.pool.keepaliveTime";
+    public static final String DB_POOL_VALIDATION_TIMEOUT = "database.pool.validationTimeout";
 }
