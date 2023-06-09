632053a68d1db1b9e7f3220d9ee93bd74403a0fe
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2023-06-09 15:49:40 +0100

    Address PR feedback

--- data/config-schema/src/main/java/net/corda/schema/configuration/DatabaseConfig.java
+++ data/config-schema/src/main/java/net/corda/schema/configuration/DatabaseConfig.java
@@ -13,6 +13,6 @@ public final class DatabaseConfig {
     public static final String DB_POOL_MIN_SIZE = "database.pool.min_size";
     public static final String DB_POOL_IDLE_TIMEOUT = "database.pool.idleTimeout";
     public static final String DB_POOL_MAX_LIFETIME = "database.pool.maxLifetime";
-    public static final String DB_POOL_KEEP_ALIVE_TIME = "database.pool.keepaliveTime";
+    public static final String DB_POOL_KEEPALIVE_TIME = "database.pool.keepaliveTime";
     public static final String DB_POOL_VALIDATION_TIMEOUT = "database.pool.validationTimeout";
 }
