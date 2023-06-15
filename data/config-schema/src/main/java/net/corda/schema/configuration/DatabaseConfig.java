7455a28a5ef1ddc6926880e0949b06d83a61d307
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2023-06-15 14:27:44 +0100

    Address PR comment

--- data/config-schema/src/main/java/net/corda/schema/configuration/DatabaseConfig.java
+++ data/config-schema/src/main/java/net/corda/schema/configuration/DatabaseConfig.java
@@ -11,8 +11,8 @@ public final class DatabaseConfig {
     public static final String JDBC_URL = "database.jdbc.url";
     public static final String DB_POOL_MAX_SIZE = "database.pool.max_size";
     public static final String DB_POOL_MIN_SIZE = "database.pool.min_size";
-    public static final String DB_POOL_IDLE_TIMEOUT_SECS = "database.pool.idleTimeoutSecs";
-    public static final String DB_POOL_MAX_LIFETIME_SECS = "database.pool.maxLifetimeSecs";
-    public static final String DB_POOL_KEEPALIVE_TIME_SECS = "database.pool.keepaliveTimeSecs";
-    public static final String DB_POOL_VALIDATION_TIMEOUT_SECS = "database.pool.validationTimeoutSecs";
+    public static final String DB_POOL_IDLE_TIMEOUT_SECONDS = "database.pool.idleTimeoutSeconds";
+    public static final String DB_POOL_MAX_LIFETIME_SECONDS = "database.pool.maxLifetimeSeconds";
+    public static final String DB_POOL_KEEPALIVE_TIME_SECONDS = "database.pool.keepaliveTimeSeconds";
+    public static final String DB_POOL_VALIDATION_TIMEOUT_SECONDS = "database.pool.validationTimeoutSeconds";
 }
