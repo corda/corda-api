978a3754ce7c7108556e09f4287e4f144a5b1d84
omar.awad <omar.awad@r3.com>
2023-06-28 04:21:47 +0100

    CORE-14643 Add temporarily topics for incoming traffic to Flow processor

--- data/topic-schema/src/main/java/net/corda/schema/Schemas.java
+++ data/topic-schema/src/main/java/net/corda/schema/Schemas.java
@@ -124,6 +124,9 @@ public final class Schemas {
         public static final String FLOW_MAPPER_START_EVENT_TOPIC = "flow.mapper.start.event";
         public static final String FLOW_MAPPER_START_EVENT_STATE_TOPIC = getStateAndEventStateTopic(FLOW_MAPPER_START_EVENT_TOPIC);
         public static final String FLOW_MAPPER_START_EVENT_DLQ_TOPIC = getDLQTopic(FLOW_MAPPER_START_EVENT_TOPIC);
+        public static final String FLOW_MAPPER_SESSION_EVENT_TOPIC = "flow.mapper.session.event";
+        public static final String FLOW_MAPPER_SESSION_EVENT_STATE_TOPIC = getStateAndEventStateTopic(FLOW_MAPPER_SESSION_EVENT_TOPIC);
+        public static final String FLOW_MAPPER_SESSION_EVENT_DLQ_TOPIC = getDLQTopic(FLOW_MAPPER_SESSION_EVENT_TOPIC);
         public static final String FLOW_MAPPER_EVENT_TOPIC = "flow.mapper.event";
         public static final String FLOW_MAPPER_EVENT_STATE_TOPIC = getStateAndEventStateTopic(FLOW_MAPPER_EVENT_TOPIC);
         public static final String FLOW_MAPPER_EVENT_DLQ_TOPIC = getDLQTopic(FLOW_MAPPER_EVENT_TOPIC);
