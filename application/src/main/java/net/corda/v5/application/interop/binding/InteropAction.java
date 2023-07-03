a7b7a742e5e2b1c52dcb87d60398bac728e63cc3
Szymon Sztuka <szymon.sztuka@r3.com>
2023-07-03 16:01:46 +0100

    CORE-14355 Interop/ Facade method returns unwrapped resul

--- application/src/main/java/net/corda/v5/application/interop/binding/InteropAction.java
+++ application/src/main/java/net/corda/v5/application/interop/binding/InteropAction.java
@@ -1,13 +1,8 @@
 package net.corda.v5.application.interop.binding;
 
-import net.corda.v5.application.interop.facade.FacadeRequest;
-import net.corda.v5.application.interop.facade.FacadeResponse;
-
 /**
  * InteropAction represents a procedure that is communicated through the help of facades.
  * The same type, {@link InteropAction}, is returned by both facade clients and facade servers.
- * Clients return a {@link ClientAction}, representing an interop request to be performed; when {@link #getResult result} is called, the
- * request is carried out and the result obtained.
  * Servers return a {@link ServerResponse} wrapping the result value directly.
  */
 public abstract class InteropAction<T> {
@@ -15,29 +10,6 @@ public abstract class InteropAction<T> {
      * @return The result of carrying out the interop action.
      */
     public abstract T getResult();
-
-    public static final class ClientAction<T> extends InteropAction<T> {
-        private final FacadeRequest request;
-        private final Processable processor;
-        private final Interpretable<T> responseInterpreter;
-
-        /**
-         * An interop action that has not yet been carried out, but will be when the caller requests the {@link #getResult result}.
-         * @param request The {@link FacadeRequest} to send to the server.
-         * @param processor An object that knows how to send a {@link FacadeRequest} to the server and obtain a {@link FacadeResponse}.
-         * @param responseInterpreter An object that knows how to translate a {@link FacadeResponse} into the {@link #getResult result} type.
-         */
-        public ClientAction(FacadeRequest request, Processable processor, Interpretable<T> responseInterpreter) {
-            this.request = request;
-            this.processor = processor;
-            this.responseInterpreter = responseInterpreter;
-        }
-
-        @Override
-        public T getResult() {
-            return responseInterpreter.interpret(processor.process(request));
-        }
-    }
 
     public static final class ServerResponse<T> extends InteropAction<T> {
         private final T result;
