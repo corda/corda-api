f830f46ebe39e629c24d5031f6a2e4376f5d80dc
eunjee.yang <eunjee.yang@r3.com>
2023-08-09 19:59:33 +0100

    CORE-15909: introduce a new "GroupParametersService"

--- ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/GroupParametersService.java
+++ ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/GroupParametersService.java
@@ -4,10 +4,10 @@ import net.corda.v5.membership.GroupParameters;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * {@link CurrentGroupParametersService} allows flows to access {@link GroupParameters} for further verification,
+ * {@link GroupParametersService} allows flows to access {@link GroupParameters} for further verification,
  * for example, accessing to check if issued money is from authoritative party in flows.
  */
-public interface CurrentGroupParametersService {
+public interface GroupParametersService {
 
     /**
      * Returns the point in time {@link GroupParameters}
