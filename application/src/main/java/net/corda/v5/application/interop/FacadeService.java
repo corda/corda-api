5f8e2b8e5eda5207c2a30865d1e72fdad5106cfb
jmacmahonr3 <james.macmahon@r3.com>
2023-08-09 14:39:23 +0100

    Wiring in InteropIdentityLookup

--- application/src/main/java/net/corda/v5/application/interop/FacadeService.java
+++ application/src/main/java/net/corda/v5/application/interop/FacadeService.java
@@ -4,7 +4,6 @@ import net.corda.v5.application.interop.facade.Facade;
 import net.corda.v5.application.interop.facade.FacadeRequest;
 import net.corda.v5.application.interop.facade.FacadeResponse;
 import net.corda.v5.base.annotations.Suspendable;
-import net.corda.v5.base.types.MemberX500Name;
 
 /**
  * Service to perform Corda to Corda interoperability calls using Facades.
@@ -16,14 +15,13 @@ public interface FacadeService {
      * on the peers from other Membership groups.
      * @param facadeId The Facade ID denoting a facade specification used as communication with a peer.
      * @param expectedType The interface implementing (all or parts) of the facade specification.
-     * @param alias MemberX500Name of the peer to be called.
-     * @param interopGroup The Interop Group (this is not MGM group) where the peer is present.
+     * @param interopIdentity InterOpIdentityInfo of the peer to be called.
      * @param <T> The interface class which the returned object implements.
      * @return An instance of Java Dynamic Proxy implementing a given interface. Any method invocation on this
      *    instance invokes a Facade method on a peer Identity denoted by an <code>alias</code> from an <code>interopGroup</code>.
      */
     @Suspendable
-    <T> T getProxy(String facadeId, Class<T> expectedType, MemberX500Name alias, String interopGroup);
+    <T> T getProxy(String facadeId, Class<T> expectedType, InterOpIdentityInfo interopIdentity);
 
     /**
      * Interprets a Facade request and invokes a matching method on the target class.
