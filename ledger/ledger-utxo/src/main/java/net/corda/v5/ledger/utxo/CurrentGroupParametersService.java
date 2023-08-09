9bc83dacced7971a01c25b21d85ccb1ffa0497f9
eunjee.yang <eunjee.yang@r3.com>
2023-08-09 16:33:46 +0100

    CORE-15909: remove getMgmKeys()

--- ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/CurrentGroupParametersService.java
+++ ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/CurrentGroupParametersService.java
@@ -3,9 +3,6 @@ package net.corda.v5.ledger.utxo;
 import net.corda.v5.membership.GroupParameters;
 import org.jetbrains.annotations.NotNull;
 
-import java.security.PublicKey;
-import java.util.List;
-
 /**
  * {@link CurrentGroupParametersService} allows flows to access {@link GroupParameters} for further verification,
  * for example, accessing to check if issued money is from authoritative party in flows.
@@ -17,10 +14,4 @@ public interface CurrentGroupParametersService {
      */
     @NotNull
     GroupParameters getCurrentGroupParameters();
-
-    /**
-     * Returns the list of {@link PublicKey} of MGM.
-     */
-    @NotNull
-    List<PublicKey> getMgmKeys();
 }
