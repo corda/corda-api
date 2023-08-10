931801458a580b05d4900b4eeacda92a4afa5dab
eunjee.yang <eunjee.yang@r3.com>
2023-08-10 10:55:22 +0100

    Revert "CORE-15909: introduce a new "GroupParametersService""
    This reverts commit 75f010cc2dab9f1e2f9e42413af0510711380f92.

--- ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/CurrentGroupParametersService.java
+++ ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/CurrentGroupParametersService.java
@@ -4,10 +4,10 @@ import net.corda.v5.membership.GroupParameters;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * {@link GroupParametersService} allows flows to access {@link GroupParameters} for further verification,
+ * {@link CurrentGroupParametersService} allows flows to access {@link GroupParameters} for further verification,
  * for example, accessing to check if issued money is from authoritative party in flows.
  */
-public interface GroupParametersService {
+public interface CurrentGroupParametersService {
 
     /**
      * Returns the point in time {@link GroupParameters}
