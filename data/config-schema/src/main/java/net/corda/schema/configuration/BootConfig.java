4fc9416de0faf7962dc65ed1e96a25e50c467586
Juan José Ramos Cassella <jujoramos@gmail.com>
2023-08-30 11:12:32 +0100

    Minor renaming and changes

--- data/config-schema/src/main/java/net/corda/schema/configuration/BootConfig.java
+++ data/config-schema/src/main/java/net/corda/schema/configuration/BootConfig.java
@@ -37,8 +37,8 @@ public final class BootConfig {
     public static final String BOOT_SECRETS = "secrets";
 
     public static final String BOOT_STATE_MANAGER = "stateManager";
-    public static final String BOOT_STATE_MANAGER_TYPE = BOOT_STATE_MANAGER + ".stateManagerType";
+    public static final String BOOT_STATE_MANAGER_TYPE = BOOT_STATE_MANAGER + ".type";
+    public static final String BOOT_STATE_MANAGER_DB_USER = BOOT_STATE_MANAGER + ".database.user";
+    public static final String BOOT_STATE_MANAGER_DB_PASS = BOOT_STATE_MANAGER + ".database.pass";
     public static final String BOOT_STATE_MANAGER_JDBC_URL = BOOT_STATE_MANAGER + ".database.jdbc.url";
-    public static final String BOOT_STATE_MANAGER_USER = BOOT_STATE_MANAGER + ".database.user";
-    public static final String BOOT_STATE_MANAGER_PASS = BOOT_STATE_MANAGER + ".database.pass";
 }
--- data/config-schema/src/main/java/net/corda/schema/configuration/StateManagerConfig.java
+++ data/config-schema/src/main/java/net/corda/schema/configuration/StateManagerConfig.java
@@ -4,13 +4,13 @@ public final class StateManagerConfig {
     private StateManagerConfig() {
     }
 
+    public static final String STATE_MANAGER = "stateManager";
+    public static final String TYPE = STATE_MANAGER + ".type";
+
     /**
      * Configuration for connecting to the state manager using database as underlying state storage.
      */
-    public static final String TYPE = "stateManager.stateManagerType";
-    public static final String DB_PASS = "stateManager.database.pass";
-    public static final String DB_USER = "stateManager.database.user";
-    public static final String JDBC_DRIVER = "stateManager.database.jdbc.driver";
-    public static final String JDBC_DRIVER_DIRECTORY = "stateManager.database.jdbc.directory";
-    public static final String JDBC_URL = "stateManager.database.jdbc.url";
+    public static final String DB_USER = STATE_MANAGER + ".database.user";
+    public static final String DB_PASS = STATE_MANAGER + ".database.pass";
+    public static final String JDBC_URL = STATE_MANAGER + ".database.jdbc.url";
 }
