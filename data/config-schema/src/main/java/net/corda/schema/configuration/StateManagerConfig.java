115ace0b5fa235e46c1002fbbe998e048009a051
Juan José Ramos Cassella <jujoramos@gmail.com>
2023-08-30 23:38:15 +0100

    Fix ConfigurationReadServiceImplTest

--- data/config-schema/src/main/java/net/corda/schema/configuration/StateManagerConfig.java
+++ data/config-schema/src/main/java/net/corda/schema/configuration/StateManagerConfig.java
@@ -4,13 +4,12 @@ public final class StateManagerConfig {
     private StateManagerConfig() {
     }
 
-    public static final String STATE_MANAGER = "stateManager";
-    public static final String TYPE = STATE_MANAGER + ".type";
+    public static final String TYPE = "type";
 
     /**
      * Configuration for connecting to the state manager using database as underlying state storage.
      */
-    public static final String DB_USER = STATE_MANAGER + ".database.user";
-    public static final String DB_PASS = STATE_MANAGER + ".database.pass";
-    public static final String JDBC_URL = STATE_MANAGER + ".database.jdbc.url";
+    public static final String DB_USER = "database.user";
+    public static final String DB_PASS = "database.pass";
+    public static final String JDBC_URL = "database.jdbc.url";
 }
