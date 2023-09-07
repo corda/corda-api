70a6b4603ab6b5b78a578483460c247b1b4db3d6
Juan José Ramos Cassella <jujoramos@gmail.com>
2023-09-07 10:59:47 +0100

    Add persistence unit name to configuration

--- data/config-schema/src/main/java/net/corda/schema/configuration/MessagingConfig.java
+++ data/config-schema/src/main/java/net/corda/schema/configuration/MessagingConfig.java
@@ -97,6 +97,7 @@ public final class MessagingConfig {
         public static final String JDBC_URL = DB_PROPERTIES + ".jdbc.url";
         public static final String JDBC_DRIVER = DB_PROPERTIES + "jdbc.driver";
         public static final String JDBC_DRIVER_DIRECTORY = DB_PROPERTIES + ".jdbc.directory";
+        public static final String JDBC_PERSISTENCE_UNIT_NAME = DB_PROPERTIES + ".jdbc.persistenceUnitName";
         public static final String JDBC_POOL_MAX_SIZE = DB_PROPERTIES + ".pool.maxSize";
         public static final String JDBC_POOL_MIN_SIZE = DB_PROPERTIES + ".pool.minSize";
         public static final String JDBC_POOL_IDLE_TIMEOUT_SECONDS = DB_PROPERTIES + ".pool.idleTimeoutSeconds";
