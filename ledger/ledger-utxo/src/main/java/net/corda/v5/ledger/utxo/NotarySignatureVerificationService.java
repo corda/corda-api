92a2cbdb895f8ea0e417a8375adcd9a243008f17
eunjee.yang <eunjee.yang@r3.com>
2023-12-11 14:32:16 +0000

    CORE-17816 update notary signature verification service api

--- ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/NotarySignatureVerificationService.java
+++ ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/NotarySignatureVerificationService.java
@@ -4,10 +4,7 @@ import net.corda.v5.application.crypto.DigitalSignatureAndMetadata;
 import net.corda.v5.base.annotations.DoNotImplement;
 import net.corda.v5.crypto.SecureHash;
 import net.corda.v5.ledger.common.transaction.TransactionSignatureException;
-import net.corda.v5.ledger.common.transaction.TransactionSignatureService;
-import net.corda.v5.ledger.utxo.transaction.filtered.UtxoFilteredTransaction;
 import org.jetbrains.annotations.NotNull;
-
 import java.security.PublicKey;
 import java.util.List;
 import java.util.Map;
@@ -16,20 +13,31 @@ import java.util.Map;
 public interface NotarySignatureVerificationService {
 
     /**
-     * Verifies the notary signatures attached to the current {@link UtxoFilteredTransaction}.
+     * Verifies given transaction by id is signed by the notary signatures.
      *
      * @param transactionId of signatures
      * @param notaryKey that is expected to be signed with
      * @param signatures to verify
-     * @param keyIdToNotaryKeys a map of keyId of notary to notaryKey
-     * @param transactionSignatureService to verify signature
-     * @throws TransactionSignatureException if the current {@link UtxoFilteredTransaction} fails to verify correctly.
+     * @param keyIdToNotaryKeys a map of keyId to notaryKeys
+     * @throws TransactionSignatureException if verification of given signatures failed.
      */
     void verifyNotarySignatures(
             @NotNull SecureHash transactionId,
             @NotNull PublicKey notaryKey,
             @NotNull List<DigitalSignatureAndMetadata> signatures,
-            @NotNull Map<String, Map<SecureHash, PublicKey>> keyIdToNotaryKeys,
-            @NotNull TransactionSignatureService transactionSignatureService
+            @NotNull Map<String, Map<SecureHash, PublicKey>> keyIdToNotaryKeys
+    );
+
+    /**
+     * Get notary public key by key id of signature.
+     *
+     * @param keyId of notary signature
+     * @param notaryKey that is expected to be signed with
+     * @param keyIdToNotaryKeys a map of keyId to notaryKeys
+     */
+    PublicKey getNotaryPublicKeyByKeyId(
+            @NotNull SecureHash keyId,
+            @NotNull PublicKey notaryKey,
+            @NotNull Map<String, Map<SecureHash, PublicKey>> keyIdToNotaryKeys
     );
 }
