748368d925930d2332d9ff78603b2fe777f0c8a2
eunjee.yang <eunjee.yang@r3.com>
2023-12-14 09:52:47 +0000

    CORE-17816 change api for verification service

--- ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/NotarySignatureVerificationService.java
+++ ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/NotarySignatureVerificationService.java
@@ -4,6 +4,7 @@ import net.corda.v5.application.crypto.DigitalSignatureAndMetadata;
 import net.corda.v5.base.annotations.DoNotImplement;
 import net.corda.v5.crypto.SecureHash;
 import net.corda.v5.ledger.common.transaction.TransactionSignatureException;
+import net.corda.v5.ledger.common.transaction.TransactionWithMetadata;
 import org.jetbrains.annotations.NotNull;
 import java.security.PublicKey;
 import java.util.List;
@@ -15,14 +16,14 @@ public interface NotarySignatureVerificationService {
     /**
      * Verifies given transaction by id is signed by the notary signatures.
      *
-     * @param transactionId that is expected to be signed over
+     * @param transaction that is expected to be signed over
      * @param notaryKey that is expected to be signed with
      * @param signatures to verify
      * @param keyIdToNotaryKeys a map of keyId to notaryKeys
      * @throws TransactionSignatureException if verification of given signatures failed.
      */
     void verifyNotarySignatures(
-            @NotNull SecureHash transactionId,
+            @NotNull TransactionWithMetadata transaction,
             @NotNull PublicKey notaryKey,
             @NotNull List<DigitalSignatureAndMetadata> signatures,
             @NotNull Map<String, Map<SecureHash, PublicKey>> keyIdToNotaryKeys
