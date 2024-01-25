5728d38c5ca707d369778f8fe3fbed51594f7870
Conal Smith <conal.smith@r3.com>
2024-01-25 12:40:56 +0000

    CORE-19029 - revert avoid abbreviations for user, pass and pool in config keys as too many cascading impacts

--- data/config-schema/src/main/java/net/corda/schema/configuration/StateManagerConfig.java
+++ data/config-schema/src/main/java/net/corda/schema/configuration/StateManagerConfig.java
@@ -33,17 +33,17 @@ public final class StateManagerConfig {
     // Database Configuration Values
     public static final class Database {
         public static final String DB_PROPERTIES = "database";
-        public static final String JDBC_USER = DB_PROPERTIES + ".username";
-        public static final String JDBC_PASS = DB_PROPERTIES + ".passworkd";
+        public static final String JDBC_USER = DB_PROPERTIES + ".user";
+        public static final String JDBC_PASS = DB_PROPERTIES + ".pass";
 
         public static final String JDBC_URL = DB_PROPERTIES + ".jdbc.url";
         public static final String JDBC_DRIVER = DB_PROPERTIES + ".jdbc.driver";
         public static final String JDBC_DRIVER_DIRECTORY = DB_PROPERTIES + ".jdbc.directory";
-        public static final String JDBC_POOL_MAX_SIZE = DB_PROPERTIES + ".connectionPool.maxSize";
-        public static final String JDBC_POOL_MIN_SIZE = DB_PROPERTIES + ".connectionPool.minSize";
-        public static final String JDBC_POOL_IDLE_TIMEOUT_SECONDS = DB_PROPERTIES + ".connectionPool.idleTimeoutSeconds";
-        public static final String JDBC_POOL_MAX_LIFETIME_SECONDS = DB_PROPERTIES + ".connectionPool.maxLifetimeSeconds";
-        public static final String JDBC_POOL_KEEP_ALIVE_TIME_SECONDS = DB_PROPERTIES + ".connectionPool.keepAliveTimeSeconds";
-        public static final String JDBC_POOL_VALIDATION_TIMEOUT_SECONDS = DB_PROPERTIES + ".connectionPool.validationTimeoutSeconds";
+        public static final String JDBC_POOL_MAX_SIZE = DB_PROPERTIES + ".pool.maxSize";
+        public static final String JDBC_POOL_MIN_SIZE = DB_PROPERTIES + ".pool.minSize";
+        public static final String JDBC_POOL_IDLE_TIMEOUT_SECONDS = DB_PROPERTIES + ".pool.idleTimeoutSeconds";
+        public static final String JDBC_POOL_MAX_LIFETIME_SECONDS = DB_PROPERTIES + ".pool.maxLifetimeSeconds";
+        public static final String JDBC_POOL_KEEP_ALIVE_TIME_SECONDS = DB_PROPERTIES + ".pool.keepAliveTimeSeconds";
+        public static final String JDBC_POOL_VALIDATION_TIMEOUT_SECONDS = DB_PROPERTIES + ".pool.validationTimeoutSeconds";
     }
 }
