7bc24f9d820b75f1861a2475ac36218fd8c42a28
Christian Sailer <christian.sailer@r3.com>
2024-01-26 16:43:33 +0000

    API changes to add order by clause to custom queries

--- ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/query/registration/VaultNamedQueryBuilder.java
+++ ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/query/registration/VaultNamedQueryBuilder.java
@@ -26,6 +26,49 @@ public interface VaultNamedQueryBuilder extends VaultNamedQueryBuilderBase {
     @NotNull
     VaultNamedQueryBuilder whereJson(@NotNull String query);
 
+
+    /**
+     * Sets an ORDER BY expression. The columnExpression needs to evaluate to a value in the vault database, i.e.
+     * either a column name or a JSON expression. The flags string allows to set order by flags, e.g. "ASC",
+     * these will be passed through to the database as is, so be careful with using database vendor specific
+     * expressions.
+     *
+     * This function can be called multiple times on one VaultNamedQueryBuilder. The order of calls matters - the first
+     * call defines the primary order, and within that the column of the second call will be used etc.
+     *
+     * @param columnExpression SQL expression that results in a value to order by
+     * @param flags ORDER BY flags, e.g. ASC or DESC.
+     * @return A builder instance with the ORDER BY field set.
+     */
+    @NotNull
+    VaultNamedQueryBuilder orderBy( @NotNull String columnExpression, String flags );
+
+    /**
+     * Sets an ORDER BY expression. The columnExpression needs to evaluate to a value in the vault database, i.e.
+     * either a column name or a JSON expression. This is equivalent
+     * to calling `oderBy(columnExpression, null);`
+     *
+     * This function can be called multiple times on one VaultNamedQueryBuilder. The order of calls matters - the first
+     * call defines the primary order, and within that the column of the second call will be used etc.
+     *
+     * @param columnExpression SQL expression that results in a value to order by
+     * @return A builder instance with the ORDER BY field set.
+     */
+    @NotNull
+    VaultNamedQueryBuilder orderBy( @NotNull String columnExpression);
+
+
+    /**
+     * Sets a flag to add the appropriate where clause to only select states that are unconsumed as of the time
+     * of the initial query. Use this in favour of adding a `visible_states.consumed is NULL` clause to the
+     * whereJson field. Note that states returned from such a query can still be consumed if they have
+     * been consumed after the initial query was executed.
+     *
+     * @return A builder instance with the unconsumed only flag set.
+     */
+    @NotNull
+    VaultNamedQueryBuilder selectUnconsumedStatesOnly();
+
     /**
      * Sets the filter function of the named query.
      * <p>
