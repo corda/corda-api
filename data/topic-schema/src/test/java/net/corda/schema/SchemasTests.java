25a71332a4701f61a7bc4c2aa6a53376a40d393c
bogdan.paunescu <bogdan.paunescu@r3.com>
2024-02-16 15:42:09 +0000

    [CORE-184741] - add checks for Java API for topic names

--- data/topic-schema/src/test/java/net/corda/schema/SchemasTests.java
+++ data/topic-schema/src/test/java/net/corda/schema/SchemasTests.java
@@ -8,6 +8,13 @@ public class SchemasTests {
     @Test
     void cryptoTopicsShouldLookNiceInJavaApi() {
         assertNotNull(Schemas.Crypto.RPC_HSM_REGISTRATION_MESSAGE_TOPIC);
+        assertNotNull(Schemas.Crypto.RPC_HSM_REGISTRATION_MESSAGE_RESPONSE_TOPIC);
         assertNotNull(Schemas.Crypto.RPC_OPS_MESSAGE_TOPIC);
+        assertNotNull(Schemas.Crypto.RPC_OPS_MESSAGE_RESPONSE_TOPIC);
+        assertNotNull(Schemas.Crypto.REKEY_MESSAGE_TOPIC);
+        assertNotNull(Schemas.Crypto.REKEY_MESSAGE_RESPONSE_TOPIC);
+        assertNotNull(Schemas.Crypto.REKEY_MESSAGE_STATUS_TOPIC);
+        assertNotNull(Schemas.Crypto.REWRAP_MESSAGE_TOPIC);
+        assertNotNull(Schemas.Crypto.REWRAP_MESSAGE_RESPONSE_TOPIC);
     }
 }
