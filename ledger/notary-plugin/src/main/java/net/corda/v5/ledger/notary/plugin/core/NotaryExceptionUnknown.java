6830c7a7d2700c43abc950357be5886196774a36
Dan Newton <dan.newton@r3.com>
2024-03-15 15:58:45 +0000

    handle uniqueness check before the time window lower bounds

--- ledger/notary-plugin/src/main/java/net/corda/v5/ledger/notary/plugin/core/NotaryExceptionUnknown.java
+++ ledger/notary-plugin/src/main/java/net/corda/v5/ledger/notary/plugin/core/NotaryExceptionUnknown.java
@@ -6,8 +6,8 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * The base class for notary exceptions that we cannot guarantee were fatal and might return with a different result
- * when retried. These type of exceptions will not invalidate the transaction immediately.
+ * The base class for notary exceptions that were fatal and might return with a different result when retried. These
+ * type of exceptions will not invalidate the transaction immediately.
  */
 @CordaSerializable
 public abstract class NotaryExceptionUnknown extends NotaryException {
