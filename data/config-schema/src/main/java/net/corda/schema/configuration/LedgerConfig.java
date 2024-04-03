3d6786cefc14cca9bceb7345aaac63cbade2578c
Filipe Oliveira <filipe.oliveira@r3.com>
2024-04-03 16:50:11 +0100

    Update

--- data/config-schema/src/main/java/net/corda/schema/configuration/LedgerConfig.java
+++ data/config-schema/src/main/java/net/corda/schema/configuration/LedgerConfig.java
@@ -13,7 +13,7 @@ public final class LedgerConfig {
     public static final String UTXO_TOKEN_CACHED_TOKEN_PAGE_SIZE = "tokens.cachedTokenPageSize";
     public static final String UTXO_TOKEN_CLAIM_TIMEOUT_SECONDS = "tokens.claimTimeoutSeconds";
     public static final String UTXO_TOKEN_CACHE_EXPIRY_PERIOD_MILLISECONDS = "tokens.tokenCacheExpiryPeriodMilliseconds";
-    public static final String UTXO_TOKEN_MIN_DB_BACKOFF_PERIOD_MILLISECONDS = "tokens.dbTokensFetchMinPeriodlMilliseconds";
-    public static final String UTXO_TOKEN_MAX_DB_BACKOFF_PERIOD_MILLISECONDS = "tokens.dbTokensFetchMaxPeriodMilliseconds";
+    public static final String UTXO_TOKEN_MIN_DB_BACKOFF_PERIOD_MILLISECONDS = "tokens.dbBackoffMinPeriodMilliseconds";
+    public static final String UTXO_TOKEN_MAX_DB_BACKOFF_PERIOD_MILLISECONDS = "tokens.dbBackoffMaxPeriodMilliseconds";
     public static final String UTXO_BACKCHAIN_BATCH_SIZE = "backchain.batchSize";
 }
