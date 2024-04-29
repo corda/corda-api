98fdb9bfb407cc84186d484e8f8ec110b339b91b
Eryk Styczynski <eryk.styczynski@r3.com>
2024-04-29 10:18:52 +0100

    CORE-16921 Add configurable maximum payload size

--- data/config-schema/src/main/java/net/corda/schema/configuration/FlowConfig.java
+++ data/config-schema/src/main/java/net/corda/schema/configuration/FlowConfig.java
@@ -9,6 +9,7 @@ public final class FlowConfig {
     public static final String SESSION_TIMEOUT_WINDOW = "session.timeout";
     public static final String SESSION_P2P_TTL = "session.p2pTTL";
     public static final String SESSION_FLOW_CLEANUP_TIME = "session.cleanupTime";
+    public static final String SESSION_FLOW_MAX_PAYLOAD = "session.maxPayloadSize";
     public static final String PROCESSING_MAX_RETRY_ATTEMPTS = "processing.maxRetryAttempts";
     public static final String PROCESSING_MAX_RETRY_WINDOW_DURATION = "processing.maxRetryWindowDuration";
     public static final String PROCESSING_MAX_RETRY_DELAY = "processing.maxRetryDelay";
