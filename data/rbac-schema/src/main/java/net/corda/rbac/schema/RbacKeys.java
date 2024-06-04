784765ce80c4a57e2194d426667a0accbad7acc7
Ben Yip <ben.yip@r3.com>
2024-06-04 09:22:36 +0100

    CORE-20534: Additional regex for use in RBAC permissions (#1654)
    * Add regex for use in rbac permissions

--- data/rbac-schema/src/main/java/net/corda/rbac/schema/RbacKeys.java
+++ data/rbac-schema/src/main/java/net/corda/rbac/schema/RbacKeys.java
@@ -37,6 +37,9 @@ public final class RbacKeys {
      */
     public static final String VNODE_STATE_REGEX = "[_a-zA-Z0-9]{3,255}";
 
+    // Status request ID for vNode creation is 12 chars long and for vNode upgrade is 36 chars long
+    public static final String VNODE_STATUS_REQ_REGEX = "(" + UUID_CHARS + "{36}|" + UUID_CHARS + "{12})";
+
     /**
      * RBAC user name regex.
      */
@@ -48,6 +51,11 @@ public final class RbacKeys {
     public static final String USER_URL_REGEX = ALLOWED_USER_URL_CHARS + "{3,200}[%40]{0,3}" + ALLOWED_USER_URL_CHARS + "{0,50}";
 
     /**
+     * CPI file checksum.
+     */
+    public static final String CPI_FILE_CHECKSUM_REGEX = UUID_CHARS + "{12}";
+
+    /**
      * Flow start client request ID.
      */
     public static final String CLIENT_REQ_REGEX = "[-._A-Za-z0-9]{1,242}";
@@ -56,4 +64,39 @@ public final class RbacKeys {
      * FQN for a flow to be started.
      */
     public static final String FLOW_NAME_REGEX = "[._$a-zA-Z0-9]{1,250}";
+
+    /**
+     * Flow state regex.
+     */
+    public static final String FLOW_STATE_REGEX = "[_a-zA-Z0-9]{3,255}";
+
+    /**
+     * Certificate usage regex.
+     */
+    public static final String CERTIFICATE_USAGE_REGEX = "(p2p-tls|p2p-session|code-signer)";
+
+    /**
+     * Tenant ID regex.
+     */
+    public static final String TENANT_ID_REGEX = "(" + UUID_CHARS + "{12}|p2p)";
+
+    /**
+     * Key ID regex.
+     */
+    public static final String KEY_ID_REGEX = UUID_CHARS + "{12}";
+
+    /**
+     * HSM category regex.
+     */
+    public static final String HSM_CATEGORY_REGEX = "[_A-Z]{2,20}";
+
+    /**
+     * Alias which keys are stored under regex.
+     */
+    public static final String ALIAS_REGEX = "[-._a-zA-Z0-9]{1,255}";
+
+    /**
+     * Key scheme regex.
+     */
+    public static final String KEY_SCHEME_REGEX = "CORDA\\.[-.A-Z0-9]{3,30}";
 }
